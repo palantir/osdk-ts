984f10cae5758bade767865563da96e4dfb54163
Eric Anderson <eanderson@palantir.com>
2023-09-27 15:41:43 -0400

    Apply copyright header

--- monorepo/eslint-config-sane/library.cjs
+++ monorepo/eslint-config-sane/library.cjs
@@ -19,6 +19,30 @@ module.exports = {
   ignorePatterns: ["node_modules/", "dist/", "lib/"],
   rules: {
     "@typescript-eslint/consistent-type-imports": "error",
+
+    "header/header": [
+      2,
+      "block",
+      [
+        "",
+        " * Copyright 2023 Palantir Technologies, Inc. All rights reserved.",
+        " *",
+        ' * Licensed under the Apache License, Version 2.0 (the "License");',
+        " * you may not use this file except in compliance with the License.",
+        " * You may obtain a copy of the License at",
+        " *",
+        " *     http://www.apache.org/licenses/LICENSE-2.0",
+        " *",
+        " * Unless required by applicable law or agreed to in writing, software",
+        ' * distributed under the License is distributed on an "AS IS" BASIS,',
+        " * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.",
+        " * See the License for the specific language governing permissions and",
+        " * limitations under the License.",
+        " ",
+      ],
+      2,
+    ],
+
     "import/no-default-export": "off",
 
     // "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
@@ -39,7 +63,7 @@ module.exports = {
     "import/no-unresolved": "off",
     "import/no-named-as-default": "error",
   },
-  plugins: ["@typescript-eslint", "import"],
+  plugins: ["@typescript-eslint", "import", "header"],
   parser: "@typescript-eslint/parser",
 
   // Obnoxiously, the only way to get eslint to check these files (and apply normal rules)
