1cbfe0ac9b5176ef23ff7a40ed3543647da50ad3
Matt Fedderly <mfedderly@palantir.com>
2023-10-26 15:57:22 -0400

    Fix compile error

--- packages/legacy-client/src/ontology-runtime/aggregations/Aggregations.ts
+++ packages/legacy-client/src/ontology-runtime/aggregations/Aggregations.ts
@@ -14,16 +14,12 @@
  * limitations under the License.
  */
 
-import type { LocalDate, Timestamp } from "../baseTypes";
+import type { BaseBucket, LocalDate, Timestamp } from "../baseTypes";
 import type { AggregationComputeStep } from "./ComputeStep";
 import type { CountOperation } from "./CountOperation";
 import type { GroupKeyType } from "./groupBy/GroupKeyType";
 import type { MetricValueType, MultipleAggregatableProperty } from "./metrics";
 
-export interface BaseBucket<K, V> {
-  key: K;
-  value: V;
-}
 export type Double = number;
 export type Rangeable = Double | Timestamp | LocalDate;
 export type MetricValue = Double | Timestamp | LocalDate;
