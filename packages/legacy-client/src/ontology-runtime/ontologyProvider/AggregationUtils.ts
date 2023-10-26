65b300c26ce57120e5bccd114a8ff92530408770
Pavel <pkondratyev@palantir.com>
2023-10-26 14:07:47 -0400

    Adding aggregation implementations

--- packages/legacy-client/src/ontology-runtime/ontologyProvider/AggregationUtils.ts
+++ packages/legacy-client/src/ontology-runtime/ontologyProvider/AggregationUtils.ts
@@ -15,7 +15,6 @@
  */
 
 import type {
-  AggregateObjectsResponseV2 as AggregationAPIResponse,
   Aggregation as ApiAggregationClause,
   AggregationGroupByV2,
   AggregationGroupByV2_Duration,
@@ -27,21 +26,16 @@ import type {
 
 import {
   type AggregationClause,
-  type AggregationResult,
-  type BucketGroup,
   type BucketKey,
   type BucketValue,
   type GroupByClause,
   type InternalBucketing,
   type InternalBucketingVisitor,
-  type Metrics,
-  type MetricValue,
   type Range,
   type Rangeable,
   visitInternalBucketing,
 } from "../aggregations";
-import { type BaseObjectType, LocalDate, Timestamp } from "../baseTypes";
-import type { SearchClause } from "../filters";
+import { LocalDate, Timestamp } from "../baseTypes";
 
 export function buildBucketObject<T>(startValue?: T, endValue?: T): {
   startValue?: T;
@@ -98,18 +92,6 @@ export function mapBucketing<
       };
     },
   } as InternalBucketingVisitor<TBucketKey, T, AggregationGroupByV2>);
-}
-
-export function mapToAggregationResponse<
-  TBucketKey extends BucketKey,
-  TBucketGroup extends BucketGroup,
-  TMetrics extends Metrics | MetricValue,
->(apiResponse: AggregationAPIResponse, body: {
-  aggregation: AggregationClause[];
-  groupBy?: Array<InternalBucketing<TBucketKey, BucketValue>>;
-  where?: SearchClause;
-}, objectType: BaseObjectType): AggregationResult<TBucketGroup, TMetrics> {
-  throw new Error("not implemented");
 }
 
 export function convertRanges(
