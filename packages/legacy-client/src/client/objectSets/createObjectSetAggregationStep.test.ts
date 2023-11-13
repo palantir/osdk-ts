8d80e1e301d45bc36fcff0c78c957f50069d1bec
Pavel <pkondratyev@palantir.com>
2023-11-13 12:17:48 -0500

    Fixing multiple aggregation properties, adding some aggregation tests

---
+++ packages/legacy-client/src/client/objectSets/createObjectSetAggregationStep.test.ts
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2023 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { createThinClient, type ThinClient } from "@osdk/api";
+import type {
+  AggregateObjectSetRequestV2,
+  AggregateObjectSetResponseV2,
+} from "@osdk/gateway/types";
+import type { MockedFunction } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { MockOntology } from "../../util/test";
+import {
+  expectFetchToBeCalledWithBody,
+  mockFetchResponse,
+} from "../../util/test/fetchUtils";
+import { MOCK_ORIGIN } from "../../util/test/mocks/mockMetadata";
+import { unwrapResultOrThrow } from "../../util/test/resultUtils";
+import { createObjectSetAggregationStep } from "./createObjectSetAggregationStep";
+
+describe(createObjectSetAggregationStep, () => {
+  let fetch: MockedFunction<typeof globalThis.fetch>;
+  let client: ThinClient<typeof MockOntology>;
+  beforeEach(() => {
+    fetch = vi.fn();
+    client = createThinClient(
+      MockOntology,
+      MOCK_ORIGIN,
+      () => "Token",
+      fetch,
+    );
+  });
+
+  it("creates aggregations", async () => {
+    const aggregatableObjectSetStep = createObjectSetAggregationStep(
+      client,
+      "Todo",
+      { type: "base", objectType: "Todo" },
+      [],
+    );
+
+    const aggregationBody: AggregateObjectSetRequestV2 = {
+      objectSet: { type: "base", objectType: "Todo" },
+      groupBy: [],
+      aggregation: [{ type: "max", name: "max", field: "points" }],
+    };
+
+    const aggregationResponse: AggregateObjectSetResponseV2 = {
+      excludedItems: 0,
+      data: [{
+        metrics: [{
+          name: "max",
+          value: 10,
+        }],
+        group: {},
+      }],
+    };
+
+    mockFetchResponse(fetch, aggregationResponse);
+    const result = await aggregatableObjectSetStep.max(a => a.points).compute();
+    expectFetchToBeCalledWithBody(
+      fetch,
+      `Ontology/objectSets/aggregate`,
+      aggregationBody,
+    );
+    const aggregationValue = unwrapResultOrThrow(result);
+    expect(aggregationValue).toEqual(10);
+  });
+
+  it("creates aggregation with builder", async () => {
+    const aggregatableObjectSetStep = createObjectSetAggregationStep(
+      client,
+      "Todo",
+      { type: "base", objectType: "Todo" },
+      [],
+    );
+
+    const aggregationBody: AggregateObjectSetRequestV2 = {
+      objectSet: { type: "base", objectType: "Todo" },
+      groupBy: [],
+      aggregation: [{ type: "max", name: "max", field: "points" }],
+    };
+
+    const aggregationResponse: AggregateObjectSetResponseV2 = {
+      excludedItems: 0,
+      data: [{
+        metrics: [{
+          name: "max",
+          value: 10,
+        }],
+        group: {},
+      }],
+    };
+
+    mockFetchResponse(fetch, aggregationResponse);
+    const result = await aggregatableObjectSetStep.aggregate(a => ({
+      max: a.points.max(),
+    })).compute();
+    expectFetchToBeCalledWithBody(
+      fetch,
+      `Ontology/objectSets/aggregate`,
+      aggregationBody,
+    );
+  });
+});
