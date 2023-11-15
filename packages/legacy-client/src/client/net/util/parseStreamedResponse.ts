c910bc063d3ee474c538fe9b25479d974cca82bc
Matt Fedderly <mfedderly@palantir.com>
2023-11-15 17:01:18 -0500

    Fix bug with parseStreamedResponse trying to decode UTF8 characters that happened to be split between chunks

--- packages/legacy-client/src/client/net/util/parseStreamedResponse.ts
+++ packages/legacy-client/src/client/net/util/parseStreamedResponse.ts
@@ -14,41 +14,111 @@
  * limitations under the License.
  */
 
+const START_TOKEN = new Uint8Array([123, 34, 100, 97, 116, 97, 34, 58, 91]); // `{"data":[`
+const OBJECT_OPEN_CHAR_CODE = 123; // '{'
+const OBJECT_CLOSE_CHAR_CODE = 125; // '}'
+
 export async function* parseStreamedResponse(
   asyncIterable: AsyncIterable<Uint8Array>,
 ) {
   const utf8decoder = new TextDecoder("utf-8");
-  let accumulatedResponse = "";
+
   let parsedStart = false;
+  let prevChunk: Uint8Array | undefined;
+  let openBracesCount = 0;
 
-  for await (const chunkValue of asyncIterable) {
-    const chunk = utf8decoder.decode(chunkValue);
-
-    accumulatedResponse += chunk;
-    if (!parsedStart && accumulatedResponse.startsWith(`{"data":[`)) {
-      accumulatedResponse = accumulatedResponse.replace(`{"data":[`, "");
+  for await (let chunk of asyncIterable) {
+    // on the first chunk, skip the expected START_TOKEN if we see it
+    let i = 0;
+    if (!parsedStart) {
       parsedStart = true;
+      if (startsWith(chunk, START_TOKEN)) {
+        i = START_TOKEN.length;
+      }
     }
 
-    let openBracesCount = 0;
-    let startPos = 0;
-    for (let i = 0; i < accumulatedResponse.length; i++) {
-      if (accumulatedResponse[i] === "{") {
-        if (openBracesCount === 0) {
-          startPos = i;
+    for (; i < chunk.length; i++) {
+      // if we aren't currently parsing an object, skip until we find the next object start
+      while (
+        openBracesCount === 0 && chunk[i] !== OBJECT_OPEN_CHAR_CODE
+        && i < chunk.length
+      ) {
+        i++;
+      }
+
+      // iterate through the chunk looking for the end of the current top level object
+      let j = i;
+      for (; j < chunk.length; j++) {
+        const c = chunk[j];
+        if (c === OBJECT_OPEN_CHAR_CODE) {
+          openBracesCount++;
+        } else if (c === OBJECT_CLOSE_CHAR_CODE) {
+          openBracesCount--;
+
+          // found a complete top level object, emit it
+          if (0 === openBracesCount) {
+            yield combineAndParse(
+              utf8decoder,
+              prevChunk,
+              chunk.slice(i, j + 1),
+            );
+
+            // if there was a prevChunk, we've consumed it now
+            prevChunk = undefined;
+
+            // advance the start index to the final '}' of the current object,
+            // which lets us start seeking the beginning of the next object
+            i = j;
+            break;
+          }
         }
-        openBracesCount++;
-      } else if (accumulatedResponse[i] === "}") {
-        openBracesCount--;
-        if (openBracesCount === 0) {
-          const jsonObject = accumulatedResponse.slice(startPos, i + 1);
-          yield JSON.parse(jsonObject);
-          accumulatedResponse = accumulatedResponse.slice(i + 1);
-          i = -1;
+      }
+
+      // if we reached the end of our chunk before finding the end of the object
+      // store off the relevant remainder of our current chunk and go grab the next one
+      if (j === chunk.length) {
+        if (!prevChunk) {
+          prevChunk = chunk.slice(i);
+        } else {
+          // unfortunately, a single object is split across more than two chunks so we need to start combining them
+          const combined = new Uint8Array(prevChunk.length + chunk.length);
+          combined.set(prevChunk);
+          combined.set(chunk, prevChunk.length);
+          prevChunk = combined;
         }
+        break;
       }
     }
   }
+}
+
+function startsWith(a: Uint8Array, b: Uint8Array) {
+  if (a.length < b.length) {
+    return false;
+  }
+
+  for (let i = 0; i < b.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function combineAndParse(
+  utf8decoder: TextDecoder,
+  prev: Uint8Array | undefined,
+  curr: Uint8Array,
+) {
+  if (!prev) {
+    return JSON.parse(utf8decoder.decode(curr));
+  }
+
+  const bytes = new Uint8Array(prev.length + curr.length);
+  bytes.set(prev);
+  bytes.set(curr, prev.length);
+  return JSON.parse(utf8decoder.decode(bytes));
 }
 
 export async function* iterateReadableStream(
