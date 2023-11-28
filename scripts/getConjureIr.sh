26df5fc6c1068c5a12af90babc6ed580784b1697
Eric Anderson <eanderson@palantir.com>
2023-11-28 13:51:22 -0500

    Add generator commands for conjure

---
+++ scripts/getConjureIr.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+mkdir -p "${SCRIPT_DIR}/../tmp"
+
+MAVEN_CONJURE_GROUP_ID="$1"
+MAVEN_CONJURE_ARTIFACT_ID="$2"
+
+CURRENT_VERSION=$( wget -q -O - "${MAVEN_CONJURE_BASE_PATH}/$(echo "$MAVEN_CONJURE_GROUP_ID" | sed 's/\./\//g')/${MAVEN_CONJURE_ARTIFACT_ID}/maven-metadata.xml" | \
+    xq -r '.metadata.version' )
+
+wget -q -O - "${MAVEN_CONJURE_BASE_PATH}/$(echo "$MAVEN_CONJURE_GROUP_ID" | sed 's/\./\//g')/${MAVEN_CONJURE_ARTIFACT_ID}/${CURRENT_VERSION}/${MAVEN_CONJURE_ARTIFACT_ID}-${CURRENT_VERSION}.conjure.json" > "${SCRIPT_DIR}/../tmp/${MAVEN_CONJURE_ARTIFACT_ID}.conjure.json"
+
