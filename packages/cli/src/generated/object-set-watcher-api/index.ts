aff9faef081546bec696c7ab933e5e6484061a49
Eric Anderson <eanderson@palantir.com>
2023-11-28 13:52:03 -0500

    GENERATED: streaming conjure objects

---
+++ packages/cli/src/generated/object-set-watcher-api/index.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2023 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export * as ObjectSetWatchService from "./ObjectSetWatchService.js";
+
+export type { AllPropertiesPropertySet } from "./AllPropertiesPropertySet.js";
+export type { Arg } from "./Arg.js";
+export type { AuditedObjectSetSubscribeRequests } from "./AuditedObjectSetSubscribeRequests.js";
+export type { AuditedObjectSetSubscribeResponse } from "./AuditedObjectSetSubscribeResponse.js";
+export type { AuditedObjectSetSubscribeResponses } from "./AuditedObjectSetSubscribeResponses.js";
+export type { BaseVersion } from "./BaseVersion.js";
+export type { BatchEnableWatcherRequest } from "./BatchEnableWatcherRequest.js";
+export type { Error } from "./Error.js";
+export type { ErrorName } from "./ErrorName.js";
+export type { FilterValue } from "./FilterValue.js";
+export type { Message } from "./Message.js";
+export type { ObjectSetChanged } from "./ObjectSetChanged.js";
+export type { ObjectSetContext } from "./ObjectSetContext.js";
+export type { ObjectSetFilterContext } from "./ObjectSetFilterContext.js";
+export type { ObjectSetRid } from "./ObjectSetRid.js";
+export type { ObjectSetStreamSubscribeRequest } from "./ObjectSetStreamSubscribeRequest.js";
+export type { ObjectSetStreamSubscribeRequests } from "./ObjectSetStreamSubscribeRequests.js";
+export type { ObjectSetSubscribeRequest } from "./ObjectSetSubscribeRequest.js";
+export type { ObjectSetSubscribeRequests } from "./ObjectSetSubscribeRequests.js";
+export type { ObjectSetSubscribeResponse } from "./ObjectSetSubscribeResponse.js";
+export type { ObjectSetSubscribeResponses } from "./ObjectSetSubscribeResponses.js";
+export type { ObjectSetUpdates } from "./ObjectSetUpdates.js";
+export type { ObjectTypeRid } from "./ObjectTypeRid.js";
+export type { PatchOffset } from "./PatchOffset.js";
+export type { PropertyId } from "./PropertyId.js";
+export type { PropertySelection } from "./PropertySelection.js";
+export type { PropertySet } from "./PropertySet.js";
+export type { PropertyTypeRid } from "./PropertyTypeRid.js";
+export type { PropertyTypeRidPropertySelection } from "./PropertyTypeRidPropertySelection.js";
+export type { QosError } from "./QosError.js";
+export type { RefreshObjectSet } from "./RefreshObjectSet.js";
+export type { RequestId } from "./RequestId.js";
+export type { SelectedPropertiesPropertySet } from "./SelectedPropertiesPropertySet.js";
+export type { StreamMessage } from "./StreamMessage.js";
+export type { SubscriptionError } from "./SubscriptionError.js";
+export type { SubscriptionId } from "./SubscriptionId.js";
+export type { SubscriptionSuccess } from "./SubscriptionSuccess.js";
+export type { UnresolvedFilterParameterId } from "./UnresolvedFilterParameterId.js";
+
+export * as object from "./object/index.js";
+export * as types from "./types/index.js";
