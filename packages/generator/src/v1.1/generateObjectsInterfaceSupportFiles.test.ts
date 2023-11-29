41556cce150abf1d03da517b2a53ffa328fd147d
Matt Fedderly <mfedderly@palantir.com>
2023-11-29 15:00:22 -0500

    ... and the generator test

--- packages/generator/src/v1.1/generateObjectsInterfaceSupportFiles.test.ts
+++ packages/generator/src/v1.1/generateObjectsInterfaceSupportFiles.test.ts
@@ -36,12 +36,21 @@ describe(generateObjectsInterfaceSupportFiles, () => {
       "import { ObjectSetAggregateArg, ObjectSetFilterArg, ObjectSetGroupByArg, ObjectSetMultipleAggregateArg, ObjectSetOrderByArg } from \\"@osdk/legacy-client\\";
       import { Todo } from \\"../Todo\\";
 
+      /** @deprecated Use ObjectSetFilterArg<Todo> instead
       export type TodoFilter = ObjectSetFilterArg<Todo>;
+      /** @deprecated Use ObjectSetOrderByArg<Todo> instead
       export type TodoOrderBy = ObjectSetOrderByArg<Todo>;
+      /** @deprecated Use ObjectSetGroupByArg<Todo> instead
       export type TodoGroupByProperties = ObjectSetGroupByArg<Todo>;
-      /** Aggregation properties for Todo. */
+      /**
+              * Aggregation properties for Todo
+              * @deprecated Use ObjectSetAggregateArg<Todo> instead
+              */
       export type TodoAggregationProperties = ObjectSetAggregateArg<Todo>;
-      /** Multiple aggregation properties for Todo. */
+      /**
+              * Multiple aggregation properties for Todo.
+              * @deprecated Use ObjectSetMultipleAggregateArg<Todo> instead
+              */
       export type TodoMultipleAggregationProperties = ObjectSetMultipleAggregateArg<Todo>;"
     `);
   });
