7f36b9c6984f5adbd0521fdaba3c90a59fbf3781
Matt Fedderly <mfedderly@palantir.com>
2023-11-30 17:06:14 -0500

    todoapp codegen

---
+++ examples/todoapp/src/generatedNoCheck/internal/@foundry/ontology-runtime/dist/baseTypes/geoshapes/DistanceMethods.ts
@@ -0,0 +1,38 @@
+import {
+  ofCentimeters as OG_ofCentimeters,
+  ofFeet as OG_ofFeet,
+  ofInches as OG_ofInches,
+  ofKilometers as OG_ofKilometers,
+  ofMeters as OG_ofMeters,
+  ofMiles as OG_ofMiles,
+  ofMillimeters as OG_ofMillimeters,
+  ofNauticalMiles as OG_ofNauticalMiles,
+  ofYards as OG_ofYards,
+} from '@osdk/legacy-client';
+
+/** @deprecated submodule imports arent public api **/
+export const ofMillimeters = OG_ofMillimeters;
+
+/** @deprecated submodule imports arent public api **/
+export const ofCentimeters = OG_ofCentimeters;
+
+/** @deprecated submodule imports arent public api **/
+export const ofMeters = OG_ofMeters;
+
+/** @deprecated submodule imports arent public api **/
+export const ofKilometers = OG_ofKilometers;
+
+/** @deprecated submodule imports arent public api **/
+export const ofInches = OG_ofInches;
+
+/** @deprecated submodule imports arent public api **/
+export const ofFeet = OG_ofFeet;
+
+/** @deprecated submodule imports arent public api **/
+export const ofYards = OG_ofYards;
+
+/** @deprecated submodule imports arent public api **/
+export const ofMiles = OG_ofMiles;
+
+/** @deprecated submodule imports arent public api **/
+export const ofNauticalMiles = OG_ofNauticalMiles;
