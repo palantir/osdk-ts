3134dc9a37f4b1177e3dbfdfb3ef14686123bac3
Matt Fedderly <mfedderly@palantir.com>
2024-01-22 09:53:39 -0500

    Make actions enumerable at runtime

---
+++ packages/client/src/actions/actions.test.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { apiServer } from "@osdk/shared.test";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import type { Client } from "../Client.js";
+import { createClient } from "../createClient.js";
+import { Ontology as MockOntology } from "../generatedNoCheck/index.js";
+import { createActionInvoker } from "./createActionInvoker.js";
+
+describe("actions", () => {
+  let client: Client<typeof MockOntology>;
+
+  beforeAll(async () => {
+    apiServer.listen();
+    client = createClient(
+      MockOntology,
+      "https://stack.palantir.com",
+      () => "myAccessToken",
+    );
+  });
+
+  afterAll(() => {
+    apiServer.close();
+  });
+
+  describe(createActionInvoker, () => {
+    it("has an enumerable list of actions", () => {
+      expect(Object.getOwnPropertyNames(client.actions)).toMatchInlineSnapshot(`
+        [
+          "promoteEmployee",
+          "promoteEmployeeObject",
+          "createOffice",
+          "createOfficeAndEmployee",
+          "moveOffice",
+          "actionTakesObjectSet",
+        ]
+      `);
+    });
+  });
+});
