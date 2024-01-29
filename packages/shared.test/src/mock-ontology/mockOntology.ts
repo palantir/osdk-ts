22a25f753833efa1a59cb1812e10d8e21fdb4a87
Matt Fedderly <mfedderly@palantir.com>
2024-01-29 17:17:32 -0500

    Update mockOntology to appear to be cli-generated

--- packages/shared.test/src/mock-ontology/mockOntology.ts
+++ packages/shared.test/src/mock-ontology/mockOntology.ts
@@ -22,7 +22,7 @@ export const MockOntology = {
   metadata: {
     ontologyRid: "",
     ontologyApiName: "Ontology",
-    userAgent: "",
+    userAgent: "@osdk/cli/0.0.0",
   },
   objects: {
     Task: {
