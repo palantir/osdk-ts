a901030965a9e1bedd30b91cb3dae3dbf5ee1df4
Stephanie Yu <stephaniey@palantir.com>
2024-02-14 16:31:06 -0500

    Update copyright to 2024

--- monorepo/eslint-config-sane/library.cjs
+++ monorepo/eslint-config-sane/library.cjs
@@ -31,7 +31,7 @@ module.exports = {
       "block",
       [
         "",
-        " * Copyright 2023 Palantir Technologies, Inc. All rights reserved.",
+        " * Copyright 2024 Palantir Technologies, Inc. All rights reserved.",
         " *",
         " * Licensed under the Apache License, Version 2.0 (the \"License\");",
         " * you may not use this file except in compliance with the License.",
