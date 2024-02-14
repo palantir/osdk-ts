f78e9522c0e568fe6ed3614a34555adaa8d42b37
Stephanie Yu <stephaniey@palantir.com>
2024-02-14 16:33:23 -0500

    use new Date

--- monorepo/eslint-config-sane/library.cjs
+++ monorepo/eslint-config-sane/library.cjs
@@ -31,7 +31,7 @@ module.exports = {
       "block",
       [
         "",
-        " * Copyright 2024 Palantir Technologies, Inc. All rights reserved.",
+        ` * Copyright ${new Date().getFullYear()} Palantir Technologies, Inc. All rights reserved.`,
         " *",
         " * Licensed under the Apache License, Version 2.0 (the \"License\");",
         " * you may not use this file except in compliance with the License.",
