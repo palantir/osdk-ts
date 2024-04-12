67ed1585f8d7970f1195731b82a2c6e4e1cb8bd3
Eric Anderson <eanderson@palantir.com>
2024-04-12 15:49:00 -0400

    Checkpoint

--- packages/tool.release/src/setupOctokit.ts
+++ packages/tool.release/src/setupOctokit.ts
@@ -33,8 +33,8 @@ export const setupOctokit = (githubToken: string) => {
           `Request quota exhausted for request ${options.method} ${options.url}`,
         );
 
-        octokit.log.info(`Retrying after ${retryAfter} seconds!`);
-        octokit.log.info(`Retry Count: ${retryCount}`);
+        octokit.log.warn(`Retrying after ${retryAfter} seconds!`);
+        octokit.log.warn(`Retry Count: ${retryCount}`);
 
         if (retryCount < 1) {
           // only retries once
