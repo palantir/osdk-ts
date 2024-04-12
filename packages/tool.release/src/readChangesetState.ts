3a38053d0c5d7d867b16b378cf2ac5e1be252cac
Eric Anderson <eanderson@palantir.com>
2024-04-12 15:18:50 -0400

    Checkpoint

--- packages/tool.release/src/readChangesetState.ts
+++ packages/tool.release/src/readChangesetState.ts
@@ -58,7 +58,8 @@ export default async function readChangesetState(
   cwd: string = process.cwd(),
 ): Promise<ChangesetState> {
   let preState = await readPreState(cwd);
-  let changesets = await readChangesets(cwd);
+  let changesets =
+    await (((readChangesets as any).default) as typeof readChangesets)(cwd);
 
   if (preState !== undefined && preState.mode === "pre") {
     let changesetsToFilter = new Set(preState.changesets);
