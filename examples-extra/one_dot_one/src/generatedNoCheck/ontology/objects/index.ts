915f575d79334f2940db3d4b36ff2e93d9c2b8ec
Zeyad Abuamer <monosec15@gmail.com>
2024-05-20 11:41:21 +0100

    Update generated Ontology

--- examples-extra/one_dot_one/src/generatedNoCheck/ontology/objects/index.ts
+++ examples-extra/one_dot_one/src/generatedNoCheck/ontology/objects/index.ts
@@ -1,6 +1,4 @@
 export type { ObjectSet } from '@osdk/legacy-client';
 export * from './ObjectTypeWithAllPropertyTypes';
-export * from './OsdkTodoProject';
-export * from './OsdkTodoTask';
 export * from './Person';
 export * from './Todo';
