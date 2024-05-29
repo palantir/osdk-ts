0ecd42b9156dad9f8b81e17a8b0319d63f4378e1
Eric Anderson <eanderson@palantir.com>
2024-05-29 08:52:56 -0400

    Generate ontology in ts 5.5-beta safe way (#355)
    * Generate ontology in ts 5.5-beta safe way
    
    * Add changeset

--- examples-extra/docs_example/src/generatedNoCheck/Ontology.ts
+++ examples-extra/docs_example/src/generatedNoCheck/Ontology.ts
@@ -3,7 +3,30 @@ import { OntologyMetadata } from './OntologyMetadata';
 import * as Actions from './ontology/actions/index';
 import * as Objects from './ontology/objects';
 
-const _Ontology = {
+export interface Ontology extends OntologyDefinition<'Employee' | 'equipment' | 'Office' | 'Todo'> {
+  metadata: OntologyMetadata;
+  objects: {
+    Employee: Objects.Employee;
+    equipment: Objects.equipment;
+    Office: Objects.Office;
+    Todo: Objects.Todo;
+  };
+  actions: {
+    completeTodo: typeof Actions.completeTodo;
+    createOffice: typeof Actions.createOffice;
+    createOfficeAndEmployee: typeof Actions.createOfficeAndEmployee;
+    createTodo: typeof Actions.createTodo;
+    moveOffice: typeof Actions.moveOffice;
+    promoteEmployee: typeof Actions.promoteEmployee;
+    promoteEmployeeObject: typeof Actions.promoteEmployeeObject;
+  };
+  queries: {
+    // TODO
+  };
+  interfaces: {};
+}
+
+export const Ontology: Ontology = {
   metadata: OntologyMetadata,
   objects: {
     Employee: Objects.Employee,
@@ -24,8 +47,4 @@ const _Ontology = {
     // TODO
   },
   interfaces: {},
-} satisfies OntologyDefinition<'Employee' | 'equipment' | 'Office' | 'Todo'>;
-
-type _Ontology = typeof _Ontology;
-export interface Ontology extends _Ontology {}
-export const Ontology = _Ontology as Ontology;
+};
