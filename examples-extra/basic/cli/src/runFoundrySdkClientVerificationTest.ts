dd6033a473d5e8bb364630eb68e3dfe4272d31bb
Eric Anderson <eanderson@palantir.com>
2024-06-07 08:08:24 -0400

    Introduce createPlatformClient (#380)

--- examples-extra/basic/cli/src/runFoundrySdkClientVerificationTest.ts
+++ examples-extra/basic/cli/src/runFoundrySdkClientVerificationTest.ts
@@ -16,7 +16,7 @@
 
 import { PalantirApiError } from "@osdk/client";
 import { Datasets } from "@osdk/internal.foundry";
-import { client } from "./client.js";
+import { platformClient as client } from "./client.js";
 import { logger } from "./logger.js";
 
 export async function runFoundrySdkClientVerificationTest(
