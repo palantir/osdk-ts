cf65948d5f3456374664c599292397bfb7258601
Eran Witkon <ewitkon@palantir.com>
2024-06-17 21:02:14 +0300

    fix style in todo app

--- examples/example-tutorial-todo-app/src/Home.tsx
+++ examples/example-tutorial-todo-app/src/Home.tsx
@@ -33,7 +33,7 @@ function Home() {
             💡 Welcome to this tutorial!
           </p>
           <p>
-            The Todo App below has been implemented with fake in memory data.
+            The To-do App is implemented with mock in-memory data.
             Can you solve how to switch it to use the Ontology SDK instead?
           </p>
         </div>
