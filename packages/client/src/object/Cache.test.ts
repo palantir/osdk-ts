d98af83ef4f8af327bcf828a42e7a9cb0038954f
Bryant Park <bryantp@palantir.com>
2024-07-10 14:08:45 -0400

    Add test to make sure copying client doesn't change the cache

--- packages/client/src/object/Cache.test.ts
+++ packages/client/src/object/Cache.test.ts
@@ -58,6 +58,14 @@ describe("AsyncCache", () => {
     expect(await cache.get(fauxClient2, "k")).toEqual("k");
   });
 
+  it("has a client and its copy access the same cache", async () => {
+    const cache = createAsyncClientCache((client: MinimalClient, key: string) =>
+      Promise.resolve(key)
+    );
+    await cache.set(fauxClient, "k", "v");
+    expect(await cache.get({ ...fauxClient }, "k")).toEqual("v");
+  });
+
   it("does not double factory", async () => {
     const resolveToKey = (client: MinimalClient, key: string) =>
       new Promise<string>((resolve, reject) => {
