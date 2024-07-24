1d18c6b4f3e33d7a996aa7c19db86c5d48fff8e1
Eric Anderson <eanderson@palantir.com>
2024-07-24 17:27:40 -0400

    Bump dev deps

--- .monorepolint.config.mjs
+++ .monorepolint.config.mjs
@@ -29,7 +29,7 @@ import {
 } from "@monorepolint/rules";
 import * as child_process from "node:child_process";
 
-const LATEST_TYPESCRIPT_DEP = "^5.5.2";
+const LATEST_TYPESCRIPT_DEP = "^5.5.4";
 
 const DELETE_SCRIPT_ENTRY = { options: [undefined], fixValue: undefined };
 
