669661db5f91dfc1f1ff643a2dd440fd6461e8b6
Timothy Leung <tleung@palantir.com>
2024-09-02 18:53:08 +0100

    Add env.test.ts

---
+++ packages/create-app.template.next-static-export/templates/src/env.test.ts
@@ -0,0 +1,33 @@
+import { afterEach, beforeEach, expect, test, vi } from "vitest";
+import { loadEnvConfig } from "@next/env";
+
+const ENV_VARS = [
+  "NEXT_PUBLIC_FOUNDRY_API_URL",
+  "NEXT_PUBLIC_FOUNDRY_CLIENT_ID",
+  "NEXT_PUBLIC_FOUNDRY_REDIRECT_URL",
+];
+
+beforeEach(() => {
+  vi.stubEnv("NODE_ENV", "production");
+});
+
+afterEach(() => {
+  vi.unstubAllEnvs();
+});
+
+for (const envVar of ENV_VARS) {
+  test.skipIf(process.env.VERIFY_ENV_PRODUCTION !== "true")(
+    `production env should contain ${envVar}`,
+    () => {
+      const env = loadEnvConfig(process.cwd());
+      expect(
+        env.combinedEnv[envVar],
+        `${envVar} should be defined`
+      ).toBeDefined();
+      expect(
+        env.combinedEnv[envVar],
+        `${envVar} should not contain placeholder value`
+      ).not.toMatch(/<.*>/);
+    }
+  );
+}
