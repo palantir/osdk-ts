0a6b2003b5f95fc9bcdd338fb5b8876b0ea02abd
Eric Anderson <eanderson@palantir.com>
2024-09-16 09:09:45 -0400

    Remove unneeded code

--- packages/e2e.test.foundry-sdk-generator/src/generatedNoCheck/@test-app2/osdk/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from './ontology/actions.js';
-export * as $Actions from './ontology/actions.js';
-export * from './ontology/interfaces.js';
-export * as $Interfaces from './ontology/interfaces.js';
-export * from './ontology/objects.js';
-export * as $Objects from './ontology/objects.js';
-export * from './ontology/queries.js';
-export * as $Queries from './ontology/queries.js';
-export { $ontologyRid } from './OntologyMetadata.js';
--- packages/e2e.test.foundry-sdk-generator/src/generatedNoCheck/@test-app2/osdk/ontology/actions.js
+++
@@ -1,4 +0,0 @@
-export { actionTakesObjectSet } from './actions/actionTakesObjectSet.js';
-export { createOffice } from './actions/createOffice.js';
-export { createOfficeAndEmployee } from './actions/createOfficeAndEmployee.js';
-export { moveOffice } from './actions/moveOffice.js';
