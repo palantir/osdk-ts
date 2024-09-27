b60e107a407701d012db1cc39e90fa6a9e203e41
Eric Anderson <eanderson@palantir.com>
2024-09-27 12:03:31 -0400

    Shared client uses .js extension now (#756)
    * Shared client uses .js extension now
    
    * Fix attw

---
+++ .changeset/early-singers-melt.md
@@ -0,0 +1,5 @@
+---
+"@osdk/shared.client": patch
+---
+
+Internally, rename file from .mjs to .js
--- packages/monorepo.tool.attw/bin/check-attw.sh
+++ packages/monorepo.tool.attw/bin/check-attw.sh
@@ -20,6 +20,7 @@ echo "Checking extra entry points: $EXTRA_ENTRY_POINTS"
 # Don't fail on exit code.
 $ATTW --pack --format json  --entrypoints . $EXTRA_ENTRY_POINTS > build/attw.json || true 
 
+cat build/attw.json
 # Check for errors, will be empty string if there are none.
 # shellcheck disable=SC2002
 errors=$(cat build/attw.json | jq --arg check_type "$BUILD_TYPE" '
@@ -31,7 +32,7 @@ errors=$(cat build/attw.json | jq --arg check_type "$BUILD_TYPE" '
         ) | map_values(if . | length == 0 then empty else . end)
     ;
 
-    filterIssues(.problems) | if (. == {}) then empty else . end
+    filterIssues(.problems | {}) | if (. == {}) then empty else . end
     ')
 
 if [[ -z "$errors" ]]; then
