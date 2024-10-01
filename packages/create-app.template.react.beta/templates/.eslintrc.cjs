10fecee2a1178be45318de23cac7a70849943e14
Eran Witkon <ewitkon@palantir.com>
2024-10-01 19:01:16 -0400

    update dev dependencies on ts2.0 beta template

--- packages/create-app.template.react.beta/templates/.eslintrc.cjs
+++ packages/create-app.template.react.beta/templates/.eslintrc.cjs
@@ -4,15 +4,33 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "import", "react"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    "import/no-unresolved": "error",
+    "import/named": "error",
+    "import/default": "error",
+    "import/namespace": "error",
+    "import/no-duplicates": "error",
+    "react/react-in-jsx-scope": "off", // Disable this rule for React 17+
+  },
+  settings: {
+    react: {
+      version: "detect", // Automatically detect the React version
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
   },
 };
