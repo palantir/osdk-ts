a8f40eb48d03069d69dd17d66acefdef0ba8982d
Amish Shah <amishs@palantir.com>
2024-10-10 14:53:56 +0100

    Copy create-app.template.tutorial-todo-aip-app -> create-app.template.tutorial-todo-aip-app.beta

---
+++ packages/create-app.template.tutorial-todo-aip-app.beta/templates/src/TaskListItem.tsx
@@ -0,0 +1,58 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import type { MockTask } from "./mocks";
+import css from "./TaskListItem.module.css";
+
+interface TaskListItemProps {
+  task: MockTask;
+  deleteTask: (task: MockTask) => Promise<void>;
+  onTaskDeleted: (taskId: string | undefined) => void;
+}
+
+function TaskListItem({ task, deleteTask, onTaskDeleted }: TaskListItemProps) {
+  const [isDeleting, setIsDeleting] = useState(false);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleClick = useCallback(async () => {
+    setIsDeleting(true);
+    try {
+      await deleteTask(task);
+    } finally {
+      onTaskDeleted(task.id);
+      setIsDeleting(false);
+    }
+  }, [deleteTask, task, onTaskDeleted]);
+
+  useEffect(() => {
+    if (textAreaRef.current) {
+      const textArea = textAreaRef.current;
+      textArea.style.height = "auto";
+      textArea.style.height = `${textArea.scrollHeight}px`;
+    }
+  }, [task.description]);
+  const cleanDescription = task.description?.trim();
+  return (
+    <li className={css.li}>
+      <input
+        type="checkbox"
+        onChange={handleClick}
+        checked={isDeleting}
+        className={css.delete}
+        title="Delete task"
+      />
+      <div className={`${css.task} ${isDeleting ? css.checked : ""}`}>
+        <span>{task.title}</span>
+        {cleanDescription != null && (
+          <textarea
+            ref={textAreaRef}
+            readOnly
+            value={task.description}
+            className={css.textArea}
+            rows={1}
+          />
+        )}
+      </div>
+    </li>
+  );
+}
+
+export default TaskListItem;
