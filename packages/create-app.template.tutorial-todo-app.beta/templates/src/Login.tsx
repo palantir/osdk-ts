e80c4a7ff97238d782f9d7449860bb150a533641
Amish Shah <amishs@palantir.com>
2024-10-10 15:13:11 +0100

    Fix up create-app.template.tutorial-todo-app.beta

--- packages/create-app.template.tutorial-todo-app.beta/templates/src/Login.tsx
+++ packages/create-app.template.tutorial-todo-app.beta/templates/src/Login.tsx
@@ -1,13 +1,13 @@
 import { useCallback, useState } from "react";
 import { Navigate } from "react-router-dom";
-import client from "./client";
+import { auth } from "./client";
 import Layout from "./Layout";
 import css from "./Login.module.css";
 
 function Login() {
   const [isLoggingIn, setIsLoggingIn] = useState(false);
   const [error, setError] = useState<string | undefined>(undefined);
-  const token = client.auth.token;
+  const token = auth.getTokenOrUndefined();
 
   const handleLogin = useCallback(async () => {
     setIsLoggingIn(true);
@@ -15,7 +15,7 @@ function Login() {
       // Initiate the OAuth flow, which will redirect the user to log into Foundry
       // Once the login has completed, the user will be redirected back to the route defined via the
       // FOUNDRY_REDIRECT_URL variable in .env.development
-      await client.auth.signIn();
+      await auth.signIn();
     } catch (e: unknown) {
       console.error(e);
       setError((e as Error).message ?? e);
