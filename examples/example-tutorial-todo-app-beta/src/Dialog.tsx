2412bf6d6e7198a1220eee0d186f810767d85d06
Amish Shah <amishs@palantir.com>
2024-10-11 10:25:52 +0100

    Add beta versions of todo app tutorial templates (#848)
    * Copy template.tutorial-todo-app -> template.tutorial-todo-app.beta
    
    * Copy create-app.template.tutorial-todo-aip-app -> create-app.template.tutorial-todo-aip-app.beta
    
    * Register the new beta templates
    
    * Fix up create-app.template.tutorial-todo-app.beta
    
    * Fix dependency names
    
    * Fix up create-app.template.tutorial-todo-aip-app.beta
    
    * Add changeset
    
    * Run pnpm generate in examples
    
    * Mark new templates as beta
    
    * Regenerate lockfile
    
    * Fixes!
    
    ---------
    
    Co-authored-by: Eric Anderson <eanderson@palantir.com>

---
+++ examples/example-tutorial-todo-app-beta/src/Dialog.tsx
@@ -0,0 +1,19 @@
+import { PropsWithChildren } from "react";
+import css from "./Dialog.module.css";
+
+interface DialogProps {
+  isOpen: boolean;
+  buttons?: React.ReactElement[];
+}
+
+function Dialog({ children, isOpen, buttons }: PropsWithChildren<DialogProps>) {
+  return (
+    <dialog open={isOpen} className={css.dialog}>
+      {children}
+      {buttons != null && buttons.length > 0 && (
+        <div className={css.buttons}>{buttons}</div>
+      )}
+    </dialog>
+  );
+}
+export default Dialog;
