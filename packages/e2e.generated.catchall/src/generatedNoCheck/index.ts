6bac6fb9629b0348ad655b6a776f5f4de07ebf02
Nihal Bhatnagar <nihal.bhatnagar@gmail.com>
2024-10-28 18:01:16 -0400

    Cherrypick interface generator fix (#935)
    * Interfaces w no props fix
    
    * changeset

--- packages/e2e.generated.catchall/src/generatedNoCheck/index.ts
+++ packages/e2e.generated.catchall/src/generatedNoCheck/index.ts
@@ -1,6 +1,6 @@
 export { actionTakesAllParameterTypes, assignEmployee1, createTodo } from './ontology/actions.js';
 export * as $Actions from './ontology/actions.js';
-export { FooInterface } from './ontology/interfaces.js';
+export { FooInterface, InterfaceNoProps } from './ontology/interfaces.js';
 export * as $Interfaces from './ontology/interfaces.js';
 export {
   BoundariesUsState,
