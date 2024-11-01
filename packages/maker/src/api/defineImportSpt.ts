f0e6cad8af0ad8ba6d11071f4aa7ad3f8fe6cd68
Alex Parson <aparson@palantir.com>
2024-11-01 09:42:20 +0000

    tests

--- packages/maker/src/api/defineImportSpt.ts
+++ packages/maker/src/api/defineImportSpt.ts
@@ -16,6 +16,9 @@
 
 import type { PropertyTypeType, SharedPropertyType } from "./types.js";
 
+/**
+ * Defines a foreign shared property type you want to take as an input to your product. The typeHint field is used for OSDK generation
+ */
 export function importSharedPropertyType(
   opts: {
     apiName: string;
