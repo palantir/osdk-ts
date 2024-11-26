b9052070bbc9822856de6c618492f4c61998a86c
Eran Witkon <ewitkon@palantir.com>
2024-11-26 10:02:05 -0500

    fix expo package.json

--- packages/create-app.template.expo.v2/templates/foundry/osdkConst.ts
+++ packages/create-app.template.expo.v2/templates/foundry/osdkConst.ts
@@ -1,9 +1,9 @@
 // Read configuration from EXPO Environment Variables
-const foundry_url = process.env.EXPO_PUBLIC_FOUNDRY_URL;
-const client_id = process.env.EXPO_PUBLIC_CLIENT_ID;
+const foundry_url = process.env.EXPO_PUBLIC_FOUNDRY_API_URL;
+const client_id = process.env.EXPO_PUBLIC_FOUNDRY_CLIENT_ID;
 
-checkEnv(foundry_url, "EXPO_PUBLIC_FOUNDRY_URL");
-checkEnv(client_id, "EXPO_PUBLIC_CLIENT_ID");
+checkEnv(foundry_url, "EXPO_PUBLIC_FOUNDRY_API_URL");
+checkEnv(client_id, "EXPO_PUBLIC_FOUNDRY_CLIENT_ID");
 
 function checkEnv(
   value: string | undefined,
