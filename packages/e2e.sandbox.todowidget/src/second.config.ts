e4839b98f155b77a5ac7401acc6c278050387730
styu <stephaniey@palantir.com>
2024-11-26 10:24:29 -0500

    [widgets] Add widget type to manifest and config (#1007)

--- packages/e2e.sandbox.todowidget/src/second.config.ts
+++ packages/e2e.sandbox.todowidget/src/second.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "@osdk/widget-client.unstable";
 
 const Config = defineConfig({
+  type: "workshop",
   rid: "ri.viewregistry..view.1234-0000-0000-0000",
   parameters: {
     headerText: {
