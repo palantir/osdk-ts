d2241f1194e30ae90381485013d95fe31be91ccc
Eran Witkon <ewitkon@palantir.com>
2024-12-03 14:55:27 -0500

    remove widget example

--- examples/example-widget-react-sdk-2.x/src/context.ts
+++
@@ -1,6 +0,0 @@
-import { useFoundryWidgetContext } from "@osdk/widget-client-react.unstable";
-import type MainConfig from "./main.config.js";
-
-export const useWidgetContext = useFoundryWidgetContext.withTypes<
-  typeof MainConfig
->();
