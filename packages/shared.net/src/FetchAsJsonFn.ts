1b60b3d33b2373eda1afaa445bfed41067d1d8b4
Eric Anderson <eanderson@palantir.com>
2024-12-05 08:52:55 -0500

    Packages use more specific versions instead of indirection through sh… (#876)
    * Packages use more specific versions instead of indirection through shared.net
    
    * fix changeset

--- packages/shared.net/src/FetchAsJsonFn.ts
+++ packages/shared.net/src/FetchAsJsonFn.ts
@@ -18,6 +18,7 @@
  * Fetches the given URL, converting the body to JSON, setting the Content-Type header to application/json,
  * and calling .json() on the response.
  */
+/** @deprecated */
 export type FetchAsJsonFn = (
   url: URL,
   requestInit: Omit<RequestInit, "body"> & { body?: object | undefined },
