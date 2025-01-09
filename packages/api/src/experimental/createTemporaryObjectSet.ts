99e976e44bdee9b88b114d790ad119e29282a634
Nihal Bhatnagar <nihal.bhatnagar@gmail.com>
2025-01-09 10:19:16 -0500

    Backports createTemporaryObjectSet (#1080)
    * Cherry-Pick PR
    
    * Fix

---
+++ packages/api/src/experimental/createTemporaryObjectSet.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2024 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { ObjectSet } from "../objectSet/ObjectSet.js";
+import type { ObjectOrInterfaceDefinition } from "../ontology/ObjectOrInterface.js";
+import type { CompileTimeMetadata } from "../ontology/ObjectTypeDefinition.js";
+import type { Experiment } from "./Experiment.js";
+
+type temporaryObjectSetFn = <Q extends ObjectOrInterfaceDefinition>(
+  objectSet: unknown extends CompileTimeMetadata<Q>["objectSet"] ? ObjectSet<Q>
+    : CompileTimeMetadata<Q>["objectSet"],
+) => Promise<string>;
+
+export const __EXPERIMENTAL__NOT_SUPPORTED_YET__createTemporaryObjectSet:
+  Experiment<
+    "2.0.12",
+    "__EXPERIMENTAL__NOT_SUPPORTED_YET__createTemporaryObjectSet",
+    { createTemporaryObjectSet: temporaryObjectSetFn }
+  > = {
+    name: "__EXPERIMENTAL__NOT_SUPPORTED_YET__createTemporaryObjectSet",
+    type: "experiment",
+    version: "2.0.12",
+  };
--- packages/client/src/createClient.ts
+++ packages/client/src/createClient.ts
@@ -16,6 +16,7 @@
 
 import type {
   ActionDefinition,
+  CompileTimeMetadata,
   InterfaceDefinition,
   ObjectOrInterfaceDefinition,
   ObjectSet,
@@ -29,10 +30,15 @@ import type {
   MinimalObjectSet,
 } from "@osdk/api/unstable";
 import {
+  __EXPERIMENTAL__NOT_SUPPORTED_YET__createTemporaryObjectSet,
   __EXPERIMENTAL__NOT_SUPPORTED_YET__getBulkLinks,
   __EXPERIMENTAL__NOT_SUPPORTED_YET__preexistingObjectSet,
   __EXPERIMENTAL__NOT_SUPPORTED_YET_subscribe,
 } from "@osdk/api/unstable";
+import {
+  OntologiesV2,
+  OntologyObjectSets,
+} from "@osdk/internal.foundry.ontologiesv2";
 import { symbolClientContext as oldSymbolClientContext } from "@osdk/shared.client";
 import { symbolClientContext } from "@osdk/shared.client2";
 import { createBulkLinksAsyncIterFactory } from "./__unstable/createBulkLinksAsyncIterFactory.js";
@@ -107,12 +113,14 @@ export function createClientInternal(
       | ObjectOrInterfaceDefinition
       | ActionDefinition<any>
       | QueryDefinition<any>
-      | Experiment<"2.0.8">,
+      | Experiment<"2.0.8">
+      | Experiment<"2.0.12">,
   >(o: T): T extends ObjectTypeDefinition ? ObjectSet<T>
     : T extends InterfaceDefinition ? MinimalObjectSet<T>
     : T extends ActionDefinition<any> ? ActionSignatureFromDef<T>
     : T extends QueryDefinition<any> ? QuerySignatureFromDef<T>
-    : T extends Experiment<"2.0.8"> ? { invoke: ExperimentFns<T> }
+    : T extends Experiment<"2.0.8"> | Experiment<"2.0.12">
+      ? { invoke: ExperimentFns<T> }
     : never
   {
     if (o.type === "object" || o.type === "interface") {
@@ -188,6 +196,25 @@ export function createClientInternal(
           } satisfies ExperimentFns<
             typeof __EXPERIMENTAL__NOT_SUPPORTED_YET_subscribe
           > as any;
+        case __EXPERIMENTAL__NOT_SUPPORTED_YET__createTemporaryObjectSet.name:
+          return {
+            createTemporaryObjectSet: async <
+              Q extends ObjectOrInterfaceDefinition,
+            >(
+              objectSet: unknown extends CompileTimeMetadata<Q>["objectSet"]
+                ? ObjectSet<Q>
+                : CompileTimeMetadata<Q>["objectSet"],
+            ) => {
+              const response = await OntologyObjectSets.createTemporary(
+                clientCtx,
+                await clientCtx.ontologyRid,
+                {
+                  objectSet: getWireObjectSet(objectSet),
+                },
+              );
+              return response.objectSetRid;
+            },
+          } as any;
       }
 
       throw new Error("not implemented");
