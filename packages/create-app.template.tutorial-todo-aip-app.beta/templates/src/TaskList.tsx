027c4d5de2793c4b6c9c808472ceaccf2ca9a23e
eran witkon <ewitkon@palantir.com>
2025-01-14 11:20:53 -0500

    fix ToDo sample to allow build on lesson level (#1083)
    * fix ToDo sample to allow build on lesson level
    
    * fix some lint issues
    
    * more lint issues
    
    * move ITask to useProjectTasks
    
    * fix mock interface mapping
    
    * fix lint issue

--- packages/create-app.template.tutorial-todo-aip-app.beta/templates/src/TaskList.tsx
+++ packages/create-app.template.tutorial-todo-aip-app.beta/templates/src/TaskList.tsx
@@ -1,10 +1,10 @@
-import type { MockProject } from "./mocks";
 import css from "./TaskList.module.css";
 import TaskListItem from "./TaskListItem";
+import { IProject } from "./useProjects";
 import { useProjectTasks } from "./useProjectTasks";
 
 interface TaskListProps {
-  project: MockProject;
+  project: IProject;
   onTaskDeleted: (taskId: string | undefined) => void;
 }
 
