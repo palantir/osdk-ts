d665147a16d494dcbda77bd850beea631773ff29
ssanjay1 <67482244+ssanjay1@users.noreply.github.com>
2025-01-15 09:21:29 -0500

    Updating platform sdk deps (#1097)
    * update platform deps
    
    * add changeset

--- packages/shared.test/src/stubs/interfaces.ts
+++ packages/shared.test/src/stubs/interfaces.ts
@@ -28,6 +28,9 @@ export const FooInterface: InterfaceType = {
   },
   links: {},
   implementedByObjectTypes: [],
+  allProperties: { fooSpt },
+  allExtendsInterfaces: [],
+  allLinks: {},
 };
 
 export const employeeInterfaceScoped = {
@@ -52,6 +55,9 @@ export const BarInterface: InterfaceType = {
   rid: "ri.interface.main.interface.2",
   extendsInterfaces: [],
   properties: {},
+  allProperties: {},
   links: {},
   implementedByObjectTypes: [],
+  allExtendsInterfaces: [],
+  allLinks: {},
 };
