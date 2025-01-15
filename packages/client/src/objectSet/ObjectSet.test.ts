234223d7ea3f65173571bc3eb25e8d4266333104
Saurav <ssanjay@palantir.com>
2025-01-15 15:51:28 -0500

    add a couple more type tests

--- packages/client/src/objectSet/ObjectSet.test.ts
+++ packages/client/src/objectSet/ObjectSet.test.ts
@@ -432,6 +432,21 @@ describe("ObjectSet", () => {
     });
   });
 
+  it("type checking struct where clauses", () => {
+    expectTypeOf(client(BgaoNflPlayer).where).toBeCallableWith({
+      $and: [{ address: { city: { $eq: "NYC" } } }, {
+        address: { zipCode: { $gte: 55555 } },
+      }],
+    });
+    client(BgaoNflPlayer).where({
+      address: {
+        addressLine1: { $containsAllTerms: "BLVD" },
+        // @ts-expect-error
+        addressLine2: { $containsAllTermsInOrder: "Apartment Number" },
+      },
+    });
+  });
+
   describe.each(["fetchPage", "fetchPageWithErrors"] as const)("%s", (k) => {
     // describe("strictNonNull: \"drop\"", () => {
     //   describe("includeRid: true", () => {
