1ca12ca29237b478786742dfa7417ffdde7a338b
Ross Court <rcourt@palantir.com>
2025-02-05 12:14:21 -0500

    Remove default export annotations

--- packages/e2e.sandbox.todowidget/src/main.config.ts
+++ packages/e2e.sandbox.todowidget/src/main.config.ts
@@ -1,40 +1,6 @@
 import { defineConfig } from "@osdk/widget-client.unstable";
 
-const config: {
-  id: "widgetOne";
-  name: "Widget One";
-  description: "First widget";
-  type: "workshop";
-  parameters: {
-    headerText: {
-      displayName: "Widget title";
-      type: "string";
-    };
-    datasetRid: {
-      displayName: "Dataset RID";
-      type: "string";
-    };
-    showWarning: {
-      displayName: "Show warning callout";
-      type: "boolean";
-    };
-    todoItems: {
-      displayName: "Todo items";
-      type: "array";
-      subType: "string";
-    };
-  };
-  events: {
-    updateHeader: {
-      displayName: "Update header";
-      parameterUpdateIds: ["headerText"];
-    };
-    updateTodoItems: {
-      displayName: "Update todo items";
-      parameterUpdateIds: ["todoItems"];
-    };
-  };
-} = defineConfig({
+export default defineConfig({
   id: "widgetOne",
   name: "Widget One",
   description: "First widget",
@@ -69,5 +35,3 @@ const config: {
     },
   },
 });
-
-export default config;
--- packages/e2e.sandbox.todowidget/src/second.config.ts
+++ packages/e2e.sandbox.todowidget/src/second.config.ts
@@ -1,27 +1,6 @@
 import { defineConfig } from "@osdk/widget-client.unstable";
 
-const Config: {
-  id: "widgetTwo";
-  name: "Widget Two";
-  description: "Second widget";
-  type: "workshop";
-  parameters: {
-    headerText: {
-      displayName: "Widget title";
-      type: "string";
-    };
-    showWarning: {
-      displayName: "Show warning callout";
-      type: "boolean";
-    };
-  };
-  events: {
-    updateHeader: {
-      displayName: "Update header";
-      parameterUpdateIds: ["headerText"];
-    };
-  };
-} = defineConfig({
+export default defineConfig({
   id: "widgetTwo",
   name: "Widget Two",
   description: "Second widget",
@@ -43,5 +22,3 @@ const Config: {
     },
   },
 });
-
-export default Config;
