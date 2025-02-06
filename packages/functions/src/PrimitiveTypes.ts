9a6d9dac88e174a4528d11aa8b5bd3fcfddc4c7a
Saurav <ssanjay@palantir.com>
2025-02-06 15:59:26 -0500

    rename

--- packages/functions/src/PrimitiveTypes.ts
+++ packages/functions/src/PrimitiveTypes.ts
@@ -19,7 +19,9 @@ export type Float<T extends number = number> = T & { __floatBrand?: void };
 export type Double<T extends number = number> = T & { __doubleBrand?: void };
 export type Long<T extends string = string> = T & { __longBrand?: void };
 
-export type DateISO<T extends string = string> = T & { __dateBrand?: void };
-export type TimestampISO<T extends string = string> = T & {
+export type DateISOString<T extends string = string> = T & {
+  __dateBrand?: void;
+};
+export type TimestampISOString<T extends string = string> = T & {
   __timestampBrand?: void;
 };
