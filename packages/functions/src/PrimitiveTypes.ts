2e0142cd9c5b03219f20f9a74d88debf13607e37
Saurav <ssanjay@palantir.com>
2025-02-06 14:01:01 -0500

    add more primitive types

--- packages/functions/src/PrimitiveTypes.ts
+++ packages/functions/src/PrimitiveTypes.ts
@@ -14,7 +14,12 @@
  * limitations under the License.
  */
 
-export type Integer<T extends number = number> = T & { __integerBrand: void };
-export type Float<T extends number = number> = T & { __floatBrand: void };
-export type Double<T extends number = number> = T & { __doubleBrand: void };
-export type Long<T extends string = string> = T & { __longBrand: void };
+export type Integer<T extends number = number> = T & { __integerBrand?: void };
+export type Float<T extends number = number> = T & { __floatBrand?: void };
+export type Double<T extends number = number> = T & { __doubleBrand?: void };
+export type Long<T extends string = string> = T & { __longBrand?: void };
+
+export type DateISO<T extends string = string> = T & { __dateBrand?: void };
+export type TimestampISO<T extends string = string> = T & {
+  __timestampBrand?: void;
+};
