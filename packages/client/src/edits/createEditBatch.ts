66d4e21ff6ce817aa3e70ad022db8d2801621fab
Marshall Vail <mvail@palantir.com>
2025-02-25 12:33:49 -0500

    no need to merge

--- packages/client/src/edits/createEditBatch.ts
+++ packages/client/src/edits/createEditBatch.ts
@@ -16,34 +16,29 @@
 
 import type { Client } from "../Client.js";
 import type { EditBatch } from "./EditBatch.js";
-import type { LinkLocatorKey, ObjectLocatorKey } from "./editKeys.js";
-import { linkLocatorKey, objectLocatorKey } from "./editKeys.js";
 import type {
   AddLinkEdits,
   AnyEdit,
   CreateObjectEdits,
   DeleteObjectEdits,
-  LinkEdits,
-  ObjectEdits,
   RemoveLinkEdits,
   UpdateObjectEdits,
 } from "./types.js";
 
 class InMemoryEditBatch<X extends AnyEdit = never> implements EditBatch<X> {
-  private objectEdits: { [key: ObjectLocatorKey]: ObjectEdits<X> } = {};
-  private linkEdits: { [key: LinkLocatorKey]: LinkEdits<X> } = {};
+  private edits: X[] = [];
 
   public link<L extends AddLinkEdits<X>>(
     source: L["source"],
     apiName: L["apiName"],
     target: L["target"],
   ): void {
-    this.linkEdits[linkLocatorKey(source, apiName, target)] = {
+    this.edits.push({
       type: "addLink",
       source,
       apiName,
       target,
-    } as L;
+    } as L);
   }
 
   public unlink<L extends RemoveLinkEdits<X>>(
@@ -51,100 +46,44 @@ class InMemoryEditBatch<X extends AnyEdit = never> implements EditBatch<X> {
     apiName: L["apiName"],
     target: L["target"],
   ): void {
-    this.linkEdits[linkLocatorKey(source, apiName, target)] = {
+    this.edits.push({
       type: "removeLink",
       source,
       apiName,
       target,
-    } as L;
+    } as L);
   }
 
   public create<O extends CreateObjectEdits<X>>(
     obj: O["obj"],
     properties: O["properties"],
   ): void {
-    const editKey = objectLocatorKey({
-      apiName: obj.apiName,
-      primaryKey: properties[obj.__DefinitionMetadata.primaryKeyApiName],
-    });
-    const previousEdit = this.objectEdits[editKey];
-    if (!previousEdit) {
-      this.objectEdits[editKey] = {
-        type: "createObject",
-        obj,
-        properties,
-      } as O;
-      return;
-    }
-
-    switch (previousEdit.type) {
-      case "createObject":
-        throw new Error(
-          `An object with this locator has already been created: ${
-            JSON.stringify(obj)
-          }`,
-        );
-      case "deleteObject":
-        throw new Error(
-          `An object with this locator has already been deleted: ${
-            JSON.stringify(obj)
-          }`,
-        );
-      case "updateObject":
-        throw new Error(
-          `An object with this locator has already been updated: ${
-            JSON.stringify(obj)
-          }`,
-        );
-      default:
-        throw new Error(`Invalid edit type: ${(previousEdit as any).type}`);
-    }
+    this.edits.push({
+      type: "createObject",
+      obj,
+      properties,
+    } as O);
   }
 
   public delete<O extends DeleteObjectEdits<X>>(
     obj: O["obj"],
   ): void {
-    const editKey = objectLocatorKey(obj);
-    const previousEdit = this.objectEdits[editKey];
-    if (previousEdit.type === "createObject") {
-      delete this.objectEdits[editKey];
-      return;
-    }
-    this.objectEdits[editKey] = { type: "deleteObject", obj } as O;
+    this.edits.push({ type: "deleteObject", obj } as O);
   }
 
   public update<O extends UpdateObjectEdits<X>>(
     obj: O["obj"],
     properties: O["properties"],
   ): void {
-    const editKey = objectLocatorKey(obj);
-    const previousEdit = this.objectEdits[editKey];
-    if (!previousEdit) {
-      this.objectEdits[editKey] = {
-        type: "updateObject",
-        obj,
-        properties,
-      } as O;
-      return;
-    }
-    if (previousEdit.type === "deleteObject") {
-      throw new Error(
-        `An object with this locator has already been deleted: ${
-          JSON.stringify(obj)
-        }`,
-      );
-    }
-    this.objectEdits[editKey] = {
-      ...previousEdit,
-      properties: { ...previousEdit.properties, ...properties },
-    };
+    this.edits.push({
+      type: "updateObject",
+      obj,
+      properties,
+    } as O);
   }
 
   public getEdits(): X[] {
-    return [
-      ...Object.values(this.objectEdits),
-      ...Object.values(this.linkEdits),
-    ];
+    return this.edits;
   }
 }
 
