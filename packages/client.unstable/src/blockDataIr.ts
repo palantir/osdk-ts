0d74541d30d1570eb8d4211ff84b908f7d202411
Alex Parson <aparson@palantir.com>
2025-02-27 14:19:39 -0500

    Fix tests

--- packages/client.unstable/src/blockDataIr.ts
+++ packages/client.unstable/src/blockDataIr.ts
@@ -79,10 +79,13 @@ export interface OntologyIrInterfaceType
       allExtendsInterfaces: string[];
       links: OntologyIrInterfaceLinkType[];
       allLinks: OntologyIrInterfaceLinkType[];
-      propertiesV2: {
-        sharedPropertyType: OntologyIrSharedPropertyType;
-        required: boolean;
-      }[];
+      propertiesV2: Record<
+        ObjectTypeFieldApiName,
+        {
+          sharedPropertyType: OntologyIrSharedPropertyType;
+          required: boolean;
+        }
+      >;
     }>
 {}
 
