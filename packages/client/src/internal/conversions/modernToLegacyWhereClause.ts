d6d6df5b31711c3a81bb081a485871e2ceb8aa93
Nihal Bhatnagar <nihalb@palantir.com>
2025-02-28 12:53:52 -0500

    Update where clause errors

--- packages/client/src/internal/conversions/modernToLegacyWhereClause.ts
+++ packages/client/src/internal/conversions/modernToLegacyWhereClause.ts
@@ -176,7 +176,7 @@ function handleWherePair(
   invariant(
     !hasDollarSign
       || keysOfFilter.length === 1,
-    "WhereClause Filter with multiple clauses isn't allowed",
+    "A WhereClause Filter with multiple clauses/fields is not allowed. Instead, use an 'and' clause to combine multiple filters.",
   );
 
   if (!hasDollarSign) {
