17ae0cecbe011ac0c9302dd532aace3e85a9cc70
Alex Parson <aparson@palantir.com>
2025-03-21 10:19:41 -0400

    remove stuff

--- examples/example-next-static-export-sdk-2.x/.next/static/chunks/pages/_app-452001c19c3b7761.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{3968:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(9039)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(3968),n(501))),_N_E=_.O()}]);
