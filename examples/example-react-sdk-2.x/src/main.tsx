929b2a44431f76d5fb348880a4207c267ce2c89e
Zeyad Abuamer <zeyadka@palantir.com>
2025-03-27 16:28:57 +0000

    lint

--- examples/example-react-sdk-2.x/src/main.tsx
+++ examples/example-react-sdk-2.x/src/main.tsx
@@ -2,8 +2,8 @@ import { OsdkProvider } from "@osdk/react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import { router } from "./router";
 import client from "./client";
+import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <OsdkProvider client={client}>
