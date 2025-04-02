b09fc319c5e609b338e210885c416d50d562f436
Zeyad Abuamer <zeyadka@palantir.com>
2025-04-02 17:11:34 +0100

    Update all create-app generated examples

--- examples/example-vue-sdk-1.x/src/client.ts
+++ examples/example-vue-sdk-1.x/src/client.ts
@@ -1,33 +1,31 @@
 import { FoundryClient, PublicClientAuth } from "@osdk/e2e.generated.1.1.x";
 
-const url = import.meta.env.VITE_FOUNDRY_API_URL;
-const clientId = import.meta.env.VITE_FOUNDRY_CLIENT_ID;
-const redirectUrl = import.meta.env.VITE_FOUNDRY_REDIRECT_URL;
-checkEnv(url, "VITE_FOUNDRY_API_URL");
-checkEnv(clientId, "VITE_FOUNDRY_CLIENT_ID");
-checkEnv(redirectUrl, "VITE_FOUNDRY_REDIRECT_URL");
+function getMetaTagContent(tagName: string): string {
+  const elements = document.querySelectorAll(`meta[name="${tagName}"]`);
+  const element = elements.item(elements.length - 1);
+  const value = element ? element.getAttribute("content") : null;
+  if (value == null) {
+    throw new Error(`Meta tag ${tagName} not found`);
+  }
+  return value;
+}
+
+const foundryUrl = getMetaTagContent("osdk-foundryUrl");
+const clientId = getMetaTagContent("osdk-clientId");
+const redirectUrl = getMetaTagContent("osdk-redirectUrl");
 const scopes = [
   "api:ontologies-read",
   "api:ontologies-write",
 ];
 
-function checkEnv(
-  value: string | undefined,
-  name: string,
-): asserts value is string {
-  if (value == null) {
-    throw new Error(`Missing environment variable: ${name}`);
-  }
-}
-
 /**
  * Initialize the client to interact with the Ontology SDK
  */
 const client = new FoundryClient({
-  url,
+  foundryUrl,
   auth: new PublicClientAuth({
     clientId,
-    url,
+    foundryUrl,
     redirectUrl,
     scopes,
   }),
