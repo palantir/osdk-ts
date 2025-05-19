b7ed5c389d98706a952d9f6914ce91620f0f22ee
Nihal Bhatnagar <nihalb@palantir.com>
2025-05-19 16:11:33 -0400

    Hold for example

---
+++ packages/cli.internal/bin/osdk.mjs
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+// @ts-check
+
+import { cli } from "../build/esm/index.js";
+cli();
---
+++ packages/cli.internal/src/cli.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2025 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import typescript from "@osdk/cli.cmd.typescript";
+import { ExitProcessError, getYargsBase } from "@osdk/cli.common";
+import { consola } from "consola";
+
+export async function cli(args: string[] = process.argv): Promise<
+  Record<string, unknown> | undefined
+> {
+  consola.info(
+    `Palantir OSDK CLI ${process.env.PACKAGE_VERSION}\n`,
+  );
+
+  const base = getYargsBase(args);
+
+  // Special handling where failures happen before yargs does its error handling within .fail
+  try {
+    return await base
+      .command(typescript).parseAsync();
+  } catch (err) {
+    if (err instanceof ExitProcessError) {
+      consola.error(err);
+    }
+  }
+}
