e33e80c7446ad427d950ca71075da56755814864
Eran Witkon <ewitkon@palantir.com>
2025-05-19 12:26:56 -0400

    remove file

--- src/generated/export_files_are_generated_correctly/index.ts
+++
@@ -1,3 +0,0 @@
-export { myObject } from './build/object-types/myObject.ts';
-export { myInterface } from './build/interface-types/myInterface.ts';
-export { mySpt } from './build/shared-property-types/mySpt.ts';
