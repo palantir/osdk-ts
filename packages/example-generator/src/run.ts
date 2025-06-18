6757de383298edf925c34074e34bad1871e02eca
Amish Shah <amishs@palantir.com>
2025-06-18 13:44:36 +0100

    Better find and replace

--- packages/example-generator/src/run.ts
+++ packages/example-generator/src/run.ts
@@ -337,17 +337,17 @@ const UPDATE_PACKAGE_JSON: Mutator = {
       )
       .replace(
         // Use locally generated SDK in the monorepo
-        /"@osdk\/widget.client-react.unstable": "\^.*?"/,
+        /"@osdk\/widget.client-react": "\^.*?"/,
         `"@osdk/widget.client-react": "workspace:*"`,
       )
       .replace(
         // Use locally generated SDK in the monorepo
-        /"@osdk\/widget.client.unstable": "\^.*?"/,
+        /"@osdk\/widget.client": "\^.*?"/,
         `"@osdk/widget.client": "workspace:*"`,
       )
       .replace(
         // Use locally generated SDK in the monorepo
-        /"@osdk\/widget.vite-plugin.unstable": "\^.*?"/,
+        /"@osdk\/widget.vite-plugin": "\^.*?"/,
         `"@osdk/widget.vite-plugin": "workspace:*"`,
       )
       .replace(
