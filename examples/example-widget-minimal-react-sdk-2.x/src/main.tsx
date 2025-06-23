0631f37ccb6acdae3c2e20acd22b04bafb97abae
Timothy Leung <tleung@palantir.com>
2025-06-23 11:21:16 +0100

    Use root id rather than body for widgets react-dom

--- examples/example-widget-minimal-react-sdk-2.x/src/main.tsx
+++ examples/example-widget-minimal-react-sdk-2.x/src/main.tsx
@@ -1,14 +1,15 @@
 import "@blueprintjs/core/lib/css/blueprint.css";
+import "./main.css";
 
 import { FoundryWidget } from "@osdk/widget.client-react";
 import { createRoot } from "react-dom/client";
 import MainConfig from "./main.config.js";
 import { Widget } from "./Widget.js";
 
-const root = document.querySelector("body")!;
+const root = document.getElementById("root")!;
 
 createRoot(root).render(
   <FoundryWidget config={MainConfig}>
     <Widget />
-  </FoundryWidget>,
+  </FoundryWidget>
 );
