8d6955598472e68bfb494408969688c0d0e4ebce
asaliling <152913317+asaliling@users.noreply.github.com>
2025-07-02 16:47:41 -0400

    Add hydrateObjectSetFromObjectRidsUtil (#1684)
    * add hydrateObjectSetFromObjectRidsUtil
    
    * add changeset
    
    * explicit return type
    
    * update comment

---
+++ packages/client/src/public-utils/hydrateObjectSetFromObjectRids.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2025 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { ObjectOrInterfaceDefinition, ObjectSet } from "@osdk/api";
+import type { Client } from "../Client.js";
+import { additionalContext } from "../Client.js";
+import { createObjectSet } from "../objectSet/createObjectSet.js";
+
+/**
+ * Creates an OSDK object set from object RIDs.
+ * @param client - An OSDK client.
+ * @param definition - An OSDK object or interface definition.
+ * @param rids - An array of object RIDs.
+ * @returns An OSDK object set.
+ */
+export function hydrateObjectSetFromObjectRids<
+  T extends ObjectOrInterfaceDefinition,
+>(client: Client, definition: T, rids: string[]): ObjectSet<T> {
+  return createObjectSet(definition, client[additionalContext], {
+    type: "intersect",
+    objectSets: [
+      {
+        type: "base",
+        objectType: definition.apiName,
+      },
+      {
+        type: "static",
+        objects: rids,
+      },
+    ],
+  });
+}
