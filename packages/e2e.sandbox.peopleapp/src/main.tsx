63874b9d48b99c9a13d0c12b8b5615c1220a4a6f
Eric Anderson <eanderson@palantir.com>
2025-07-30 14:34:41 -0400

    Add React Router for navigation between views
    - Install react-router-dom package
    - Create separate page components for Employees and Offices views
    - Implement routing configuration with nested routes
    - Update App component to use Outlet for rendering routes
    - Refactor navigation to use proper routing

--- packages/e2e.sandbox.peopleapp/src/main.tsx
+++ packages/e2e.sandbox.peopleapp/src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.js";
 import "./index.css";
 import { OsdkProvider2 } from "@osdk/react/experimental";
 import { $ } from "./foundryClient.js";
+import { Router } from "./router.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <OsdkProvider2 client={$}>
-      <App />
+      <Router />
     </OsdkProvider2>
   </React.StrictMode>,
 );
---
+++ packages/e2e.sandbox.peopleapp/src/router.tsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.js";
+import { EmployeesPage } from "./pages/EmployeesPage.js";
+import { OfficesPage } from "./pages/OfficesPage.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <EmployeesPage />,
+      },
+      {
+        path: "/employees",
+        element: <EmployeesPage />,
+      },
+      {
+        path: "/offices",
+        element: <OfficesPage />,
+      },
+    ],
+  },
+]);
+
+export function Router() {
+  return <RouterProvider router={router} />;
+}
