5661645b9846711920422a67fbd25423a703e9f4
Eric Anderson <eanderson@palantir.com>
2025-07-30 14:21:49 -0400

    clean up offices ui

--- packages/e2e.sandbox.peopleapp/src/components/Button.tsx
+++ packages/e2e.sandbox.peopleapp/src/components/Button.tsx
@@ -1,23 +1,51 @@
 import React from "react";
 
 export function Button(
-  { children, onClick, disabled }: {
+  {
+    children,
+    onClick,
+    disabled,
+    active = false,
+    variant = "primary",
+    type = "button",
+  }: {
     children: React.ReactNode;
     onClick?: () => void;
     disabled?: boolean;
+    active?: boolean;
+    variant?: "primary" | "secondary" | "tab";
+    type?: "button" | "submit" | "reset";
   },
 ) {
   return (
     <button
-      className="
-      text-sm
-      bg-blue-500 hover:bg-blue-700 
-      text-white font-bold 
-      py-2 px-4 
-      rounded-sm aria-disabled:bg-gray-300 aria-disabled:cursor-default"
+      className={`
+        text-sm font-bold py-2 px-4 rounded-sm
+        ${variant === "tab" ? "" : "text-white"}
+        ${disabled ? "bg-gray-300 cursor-default" : ""}
+        ${
+        !disabled && variant === "primary" && !active
+          ? "bg-blue-500 hover:bg-blue-700"
+          : ""
+      }
+        ${
+        !disabled && variant === "secondary" && !active
+          ? "bg-gray-200 hover:bg-gray-300 text-gray-800"
+          : ""
+      }
+        ${
+        !disabled && variant === "tab" && !active
+          ? "bg-gray-200 hover:bg-gray-300"
+          : ""
+      }
+        ${active && variant === "tab" ? "bg-blue-500 text-white" : ""}
+        ${active && variant !== "tab" ? "bg-blue-700" : ""}
+      `}
       aria-disabled={disabled}
       disabled={disabled}
+      aria-pressed={active}
       onClick={onClick}
+      type={type}
     >
       {children}
     </button>
