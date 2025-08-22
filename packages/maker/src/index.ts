3ff44dae2c2640c665a8685f8f7a3a58149c809d
ethanai22 <38924288+ethanai22@users.noreply.github.com>
2025-08-22 11:36:16 -0400

    Existing object types as action parameters in OAC (maker-experimental) (#1846)

--- packages/maker/src/index.ts
+++ packages/maker/src/index.ts
@@ -30,11 +30,20 @@ export { importSharedPropertyType } from "./api/defineImportSpt.js";
 export { defineInterface } from "./api/defineInterface.js";
 export { defineInterfaceLinkConstraint } from "./api/defineInterfaceLinkConstraint.js";
 export { defineLink } from "./api/defineLink.js";
-export { defineObject } from "./api/defineObject.js";
-export { defineOntology } from "./api/defineOntology.js";
+export {
+  convertToDisplayName,
+  convertToPluralDisplayName,
+  defineObject,
+} from "./api/defineObject.js";
+export {
+  addNamespaceIfNone,
+  defineOntology,
+  dumpOntologyFullMetadata,
+} from "./api/defineOntology.js";
 export { defineSharedPropertyType } from "./api/defineSpt.js";
 export { defineValueType } from "./api/defineValueType.js";
 export { importOntologyEntity } from "./api/importOntologyEntity.js";
+export { convertType } from "./api/propertyConversionUtils.js";
 export type {
   ActionParameterAllowedValues,
   ActionParameterConditionalOverride,
@@ -49,7 +58,9 @@ export type {
   InterfaceType,
   LinkType,
   MappingValue,
+  ObjectPropertyType,
   ObjectType,
+  PropertyTypeType,
   SectionConditionalOverride,
   SharedPropertyType,
   SubmissionMetadata,
