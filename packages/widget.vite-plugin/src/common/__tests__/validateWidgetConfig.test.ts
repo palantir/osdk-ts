44cf329ee20564f3bc85d2785d61c654b1ec43cd
Amish Shah <amishs@palantir.com>
2025-09-03 14:27:04 +0100

    [widgets] enforce camelCase for widget/parameter IDs

---
+++ packages/widget.vite-plugin/src/common/__tests__/validateWidgetConfig.test.ts
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2025 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { ParameterConfig, WidgetConfig } from "@osdk/widget.api";
+import { describe, expect, test } from "vitest";
+import { validateWidgetConfig } from "../validateWidgetConfig.js";
+
+describe("validateWidgetConfig", () => {
+  test("accepts valid widget configuration", () => {
+    const validConfig: WidgetConfig<ParameterConfig> = {
+      id: "testWidget",
+      name: "Test Widget",
+      description: "A test widget",
+      type: "workshop",
+      parameters: {
+        paramOne: {
+          displayName: "Parameter One",
+          type: "string",
+        },
+        paramTwo: {
+          displayName: "Parameter Two",
+          type: "string",
+        },
+      },
+      events: {
+        updateParameters: {
+          displayName: "Update Parameters",
+          parameterUpdateIds: ["paramOne", "paramTwo"],
+        },
+      },
+    };
+
+    expect(() => validateWidgetConfig(validConfig)).not.toThrow();
+  });
+
+  test("throws for widget id that exceeds max length", () => {
+    const invalidConfig = getValidConfig();
+    invalidConfig.id = "a".repeat(101);
+
+    expect(() => validateWidgetConfig(invalidConfig)).toThrow(
+      "Widget id length can be at most 100",
+    );
+  });
+
+  test("throws for widget id that is not camelCase", () => {
+    const testCases = [
+      { id: "TestWidget", message: "does not match allowed pattern" },
+      { id: "test-widget", message: "does not match allowed pattern" },
+      { id: "test_widget", message: "does not match allowed pattern" },
+      { id: "1testWidget", message: "does not match allowed pattern" },
+    ];
+
+    for (const { id, message } of testCases) {
+      const invalidConfig = getValidConfig();
+      invalidConfig.id = id;
+      expect(() => validateWidgetConfig(invalidConfig)).toThrow(message);
+    }
+  });
+
+  test("throws for widget name that exceeds max length", () => {
+    const invalidConfig = getValidConfig();
+    invalidConfig.name = "A".repeat(101);
+
+    expect(() => validateWidgetConfig(invalidConfig)).toThrow(
+      "Widget name length can be at most 100",
+    );
+  });
+
+  test("throws for widget description that exceeds max length", () => {
+    const invalidConfig = getValidConfig();
+    invalidConfig.description = "A".repeat(251);
+
+    expect(() => validateWidgetConfig(invalidConfig)).toThrow(
+      "Widget description length can be at most 250",
+    );
+  });
+
+  test("accepts undefined widget description", () => {
+    const validConfig = getValidConfig();
+    delete validConfig.description;
+
+    expect(() => validateWidgetConfig(validConfig)).not.toThrow();
+  });
+
+  test("throws for parameter id that is not camelCase", () => {
+    const invalidConfig = getValidConfig();
+    invalidConfig.parameters = {
+      "Invalid-Param": {
+        displayName: "Invalid Parameter",
+        type: "string",
+      },
+    };
+
+    expect(() => validateWidgetConfig(invalidConfig)).toThrow(
+      "Parameter id \"Invalid-Param\" must be in camelCase format",
+    );
+  });
+
+  test("throws for parameter id that exceeds max length", () => {
+    const invalidConfig = getValidConfig();
+    const longParamId = "param" + "a".repeat(96); // 101 chars total
+    invalidConfig.parameters = {
+      [longParamId]: {
+        displayName: "Long Parameter",
+        type: "string",
+      },
+    };
+
+    expect(() => validateWidgetConfig(invalidConfig)).toThrow(
+      "Parameter id length can be at most 100",
+    );
+  });
+});
+
+function getValidConfig(): WidgetConfig<ParameterConfig> {
+  return {
+    id: "testWidget",
+    name: "Test Widget",
+    description: "A test widget",
+    type: "workshop",
+    parameters: {
+      paramOne: {
+        displayName: "Parameter One",
+        type: "string",
+      },
+    },
+    events: {
+      updateParameters: {
+        displayName: "Update Parameters",
+        parameterUpdateIds: ["paramOne"],
+      },
+    },
+  };
+}
