8e0d96210b99627772e0447ae6e8d8ac249dfed6
Amish Shah <amishs@palantir.com>
2025-10-03 15:20:02 +0100

    Validate valid object given

--- packages/widget.vite-plugin/src/common/__tests__/validateWidgetConfig.test.ts
+++ packages/widget.vite-plugin/src/common/__tests__/validateWidgetConfig.test.ts
@@ -101,6 +101,37 @@ describe("validateWidgetConfig", () => {
       "Parameter id length can be at most 100 characters",
     );
   });
+
+  test("accepts valid object set parameter", () => {
+    const validConfig = getValidConfig();
+    validConfig.parameters.myObjectSet = {
+      type: "objectSet",
+      displayName: "My Object Set",
+      objectType: {
+        type: "object",
+        apiName: "employee",
+        experimentalDoNotUseMetadata: {
+          rid: "ri.object-type.employee",
+        },
+      },
+    };
+    expect(() => validateWidgetConfig(validConfig)).not.toThrow();
+  });
+
+  test("throws for invalid object set parameter", () => {
+    const invalidConfig = getValidConfig();
+    invalidConfig.parameters.myObjectSet = {
+      type: "objectSet",
+      displayName: "My Object Set",
+      objectType: {
+        type: "object",
+        apiName: "employee",
+      },
+    };
+    expect(() => validateWidgetConfig(invalidConfig)).toThrow(
+      "ObjectSet parameter \"myObjectSet\" must have a valid rid in its metadata",
+    );
+  });
 });
 
 function getValidConfig(): WidgetConfig<ParameterConfig> {
--- packages/widget.vite-plugin/src/common/validateWidgetConfig.ts
+++ packages/widget.vite-plugin/src/common/validateWidgetConfig.ts
@@ -64,7 +64,7 @@ function validateWidgetDescription(description?: string): void {
 }
 
 function validateWidgetParameters(parameters: ParameterConfig): void {
-  for (const parameterId of Object.keys(parameters)) {
+  for (const [parameterId, parameterConfig] of Object.entries(parameters)) {
     if (parameterId.length > ID_MAX_LENGTH) {
       throw new Error(
         `Parameter id length can be at most ${ID_MAX_LENGTH} characters`,
@@ -74,6 +74,16 @@ function validateWidgetParameters(parameters: ParameterConfig): void {
       throw new Error(
         `Parameter id "${parameterId}" does not match allowed pattern (must be camelCase)`,
       );
+    }
+    if (parameterConfig.type === "objectSet") {
+      if (
+        typeof parameterConfig.objectType.experimentalDoNotUseMetadata?.rid
+          !== "string"
+      ) {
+        throw new Error(
+          `ObjectSet parameter "${parameterId}" must have a valid rid in its metadata`,
+        );
+      }
     }
   }
 }
