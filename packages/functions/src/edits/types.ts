f87be4de71f5761ebb2b16930c8f4ca997ffd522
Marshall Vail <35933248+marshallavail@users.noreply.github.com>
2025-10-09 12:27:44 -0400

    Fix vector property types for EditBatch API (#2040)
    * Fix vector property types for EditBatch API
    
    * Remove Converted
    
    * changeset
    
    * check-api

--- packages/functions/src/edits/types.ts
+++ packages/functions/src/edits/types.ts
@@ -80,7 +80,7 @@ export interface RemoveLink<
 
 type PartialForOptionalProperties<T> =
   & {
-    [K in keyof T as undefined extends T[K] ? K : never]?: T[K];
+    [K in keyof T as undefined extends T[K] ? K : never]?: NonNullable<T[K]>;
   }
   & {
     [K in keyof T as undefined extends T[K] ? never : K]-?: T[K];
