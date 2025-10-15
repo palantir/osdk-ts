b399e6bada2ec584a56bd48939b6f05402644603
Nihal Bhatnagar <nihalb@palantir.com>
2025-10-15 14:57:34 -0400

    Update OSDK

--- packages/api/src/aggregate/WhereClause.ts
+++ packages/api/src/aggregate/WhereClause.ts
@@ -25,7 +25,7 @@ import type {
 import type { BaseWirePropertyTypes } from "../ontology/WirePropertyTypes.js";
 import type { IsNever } from "../OsdkObjectFrom.js";
 import type { ArrayFilter } from "./ArrayFilter.js";
-import type { BaseFilter } from "./BaseFilter.js";
+import type { BaseFilter, EqFilter } from "./BaseFilter.js";
 import type { BooleanFilter } from "./BooleanFilter.js";
 import type { DatetimeFilter } from "./DatetimeFilter.js";
 import type { GeoFilter } from "./GeoFilter.js";
@@ -146,9 +146,11 @@ export type GeoFilter_Intersects = {
 
 type FilterFor<PD extends ObjectMetadata.Property> = PD["multiplicity"] extends
   true
-  ? (PD["type"] extends
-    "string" | "geopoint" | "geoshape" | "datetime" | "timestamp"
-    ? ArrayFilter<string>
+  ? (PD["type"] extends Record<string, BaseWirePropertyTypes>
+    ? ArrayFilter<StructArrayFilterOpts<PD["type"]>>
+    : PD["type"] extends
+      "string" | "geopoint" | "geoshape" | "datetime" | "timestamp"
+      ? ArrayFilter<string>
     : (PD["type"] extends boolean ? ArrayFilter<boolean>
       : ArrayFilter<number>))
   : PD["type"] extends Record<string, BaseWirePropertyTypes> ?
@@ -162,6 +164,16 @@ type FilterFor<PD extends ObjectMetadata.Property> = PD["multiplicity"] extends
       "double" | "integer" | "long" | "float" | "decimal" | "byte"
       ? NumberFilter
     : BaseFilter<string>); // FIXME we need to represent all types
+
+type StructArrayFilterOpts<ST extends Record<string, BaseWirePropertyTypes>> = {
+  [K in keyof ST]?: ST[K] extends
+    "string" | "geopoint" | "geoshape" | "datetime" | "timestamp"
+    ? EqFilter.$eq<string>
+    : ST[K] extends boolean ? EqFilter.$eq<boolean>
+    : ST[K] extends "double" | "integer" | "long" | "float" | "decimal" | "byte"
+      ? EqFilter.$eq<number>
+    : never;
+};
 
 type StructFilterOpts<ST extends Record<string, BaseWirePropertyTypes>> = {
   [K in keyof ST]?: FilterFor<{ "type": ST[K] }>;
