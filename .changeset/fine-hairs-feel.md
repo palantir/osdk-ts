a035fdcdfc04d61e96140bf40e546bae4b5d9055
Bryant Park <bryantp@palantir.com>
2025-11-15 17:19:15 -0500

    Media upload docs (#2184)
    * Upgrade @osdk/docs-spec-sdk to 0.13.0
    
    * Update actions snippets and add ontology edits media upload snippet
    
    * Add files via upload
    
    * Fix nestedOsdkExamplesContext test
    
    * Add files via upload

---
+++ .changeset/fine-hairs-feel.md
@@ -0,0 +1,7 @@
+---
+"@osdk/typescript-sdk-docs-examples": minor
+"@osdk/typescript-sdk-docs": minor
+"@osdk/osdk-docs-context": patch
+---
+
+Add draft of media upload docs
---
+++ packages/typescript-sdk-docs-examples/src/examples/typescript/2.6.0/uploadMediaOntologyEdits.ts
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2023 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * WARNING: This file is generated automatically by the generateExamples.ts script.
+ * DO NOT MODIFY this file directly as your changes will be overwritten.
+ */
+
+// Example: uploadMediaOntologyEdits
+
+import type { Client } from "@osdk/client";
+import { Employee } from "../../../generatedNoCheck/index.js";
+import type { Edits } from "@osdk/functions";
+import { createEditBatch, uploadMedia } from "@osdk/functions";
+
+async function createObject(client: Client): Promise<Edits.Object<Employee>[]> {
+    const batch = createEditBatch<Edits.Object<Employee>>(client);
+    
+    const blob = new Blob(["Hello, world"], { type: "text/plain" });
+    const mediaReference = await uploadMedia(client, { data: blob, fileName: "foundryFile.txt" });
+
+    // @ts-ignore
+    batch.create(Employee, {
+        // "<mediaProperty>": mediaReference,
+    });
+
+    return batch.getEdits();
+}
+
+export default createObject;
