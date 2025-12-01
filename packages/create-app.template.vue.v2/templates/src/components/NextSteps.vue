cbb02f388712de2cbc73dfd475d6d4089c2460e2
Sami Alfhaily <salfhaily@palantir.com>
2025-12-01 18:03:17 +0000

    Make vue exactly match react

---
+++ packages/create-app.template.vue.v2/templates/src/components/NextSteps.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+interface INextStep {
+  icon: string;
+  title: string;
+  subtitle: string;
+}
+
+const STEPS: INextStep[] = [
+{{^if osdkPackage}}
+  {
+    icon: "cube",
+    title: "Configure your Ontology and Platform SDKs",
+    subtitle:
+      "Use the Ontology SDK to interact with your Ontology and the Platform SDK to access Foundry APIs. Configure your SDKs from the Ontology SDK and Platform SDK tabs in Developer Console.",
+  },
+{{^if applicationUrl}}
+{{/if}}
+  {
+    icon: "cloud",
+    title: "Request a subdomain to host your application with Foundry",
+    subtitle:
+      "Register a subdomain for your website in the Website hosting tab in Developer Console. Then, add this subdomain to your redirect URLs on the OAuth & scopes tab and update your .env.production file.",
+  },
+{{/if}}
+  {
+    icon: "tag",
+    title: "Deploy your application",
+    subtitle:
+      "Generate a new tag version of your repository to deploy a new version of your applicaiton.",
+  },
+];
+</script>
+
+<template>
+  <div class="next-steps">
+    <h5 class="next-steps-header">Next steps</h5>
+    <div v-for="step in STEPS" :key="step.title" class="next-step">
+      <img :src="`/${step.icon}.svg`" class="icon" :alt="step.icon" />
+      <div class="next-step-content">
+        <h6 class="title">{{ step.title }}</h6>
+        <div class="subtitle">{{ step.subtitle }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.next-steps {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.next-steps-header {
+  align-self: baseline;
+  font-size: 16px;
+  font-weight: 600;
+}
+
+.next-step {
+  border: 1px solid #11141826;
+  border-radius: 4px;
+  display: flex;
+  gap: 15px;
+  padding: 16px;
+}
+
+.next-step-content {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  text-align: left;
+}
+
+.title {
+  font-weight: 600;
+}
+
+.subtitle {
+  color: #5f6b7c;
+}
+
+.icon {
+  height: 34px;
+}
+</style>
