96524d02d125ff2bceb0be8063385128a8f93e3a
Sami Alfhaily <60448820+samialfhaily@users.noreply.github.com>
2025-12-05 13:39:18 +0000

    [create-app] Add CLI support for bootstrapping without an OSDK (#2234)

---
+++ examples/example-expo-sdk-2.x-no-osdk/components/HapticTab.tsx
@@ -0,0 +1,18 @@
+import { BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
+import { PlatformPressable } from "@react-navigation/elements";
+import * as haptics from "expo-haptics";
+
+export function HapticTab(props: BottomTabBarButtonProps) {
+  return (
+    <PlatformPressable
+      {...props}
+      onPressIn={(ev: any) => {
+        if (process.env.EXPO_OS === "ios") {
+          // Add a soft haptic feedback when pressing down on the tabs.
+          haptics.impactAsync(haptics.ImpactFeedbackStyle.Light);
+        }
+        props.onPressIn?.(ev);
+      }}
+    />
+  );
+}
