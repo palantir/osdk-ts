16811478b13242d089adaaa97b7493389d51395d
Bryant Park <bryantp@palantir.com>
2025-12-13 19:25:34 -0500

    Move new Media docs from 2.6 to 2.1

--- packages/typescript-sdk-docs-examples/src/examples/typescript/2.1.0/index.ts
+++ packages/typescript-sdk-docs-examples/src/examples/typescript/2.1.0/index.ts
@@ -36,6 +36,32 @@
 // subscribeToObjectSetInstructions
 // See: ./subscribeToObjectSetInstructions.ts
 
+// applyAction (variations only)
+// See: ./applyAction_#hasAttachmentProperty.ts (Standard block: hasAttachmentProperty = true)
+// See: ./applyAction_^hasAttachmentProperty.ts (Inverted block: hasAttachmentProperty = false)
+// See: ./applyAction_#hasAttachmentProperty.ts (Standard block: hasAttachmentProperty = true)
+// See: ./applyAction_#hasAttachmentProperty.ts (Standard block: hasAttachmentProperty = true)
+// See: ./applyAction_#hasMediaParameter.ts (Standard block: hasMediaParameter = true)
+// See: ./applyAction_#hasMediaParameter.ts (Standard block: hasMediaParameter = true)
+// See: ./applyAction_#hasMediaParameter.ts (Standard block: hasMediaParameter = true)
+// See: ./applyAction_#hasMediaParameter.ts (Standard block: hasMediaParameter = true)
+// See: ./applyAction_#actionParameterSampleValuesV2.ts (Standard block: actionParameterSampleValuesV2 = true)
+
+// batchApplyAction (variations only)
+// See: ./batchApplyAction_#hasAttachmentProperty.ts (Standard block: hasAttachmentProperty = true)
+// See: ./batchApplyAction_^hasAttachmentProperty.ts (Inverted block: hasAttachmentProperty = false)
+// See: ./batchApplyAction_#hasAttachmentProperty.ts (Standard block: hasAttachmentProperty = true)
+// See: ./batchApplyAction_#hasAttachmentProperty.ts (Standard block: hasAttachmentProperty = true)
+// See: ./batchApplyAction_#hasMediaParameter.ts (Standard block: hasMediaParameter = true)
+// See: ./batchApplyAction_#hasMediaParameter.ts (Standard block: hasMediaParameter = true)
+// See: ./batchApplyAction_#hasMediaParameter.ts (Standard block: hasMediaParameter = true)
+// See: ./batchApplyAction_#hasMediaParameter.ts (Standard block: hasMediaParameter = true)
+// See: ./batchApplyAction_#actionParameterSampleValuesV2.ts (Standard block: actionParameterSampleValuesV2 = true)
+// See: ./batchApplyAction_#actionParameterSampleValuesV2.ts (Standard block: actionParameterSampleValuesV2 = true)
+
+// uploadMediaOntologyEdits
+// See: ./uploadMediaOntologyEdits.ts
+
 // uploadMedia
 // See: ./uploadMedia.ts
 
