b98b012e0d7bcae27d88daf18aa873cb31b018b0
Brandon Wisnicki <bwisnicki@palantir.com>
2025-12-18 16:38:19 -0500

    users list

--- packages/react/src/new/platform-apis/admin/useFoundryUsersList.ts
+++ packages/react/src/new/platform-apis/admin/useFoundryUsersList.ts
@@ -14,3 +14,81 @@
  * limitations under the License.
  */
 
+import type { Core } from "@osdk/foundry";
+import { Admin } from "@osdk/foundry";
+
+import React from "react";
+import { usePlatformQuery } from "../../../utils/usePlatformQuery.js";
+import { OsdkContext2 } from "../../OsdkContext2.js";
+
+export interface UseCurrentFoundryUserOptions {
+  /**
+   * Enable or disable the query.
+   *
+   * When `false`, the query will not automatically execute.
+   *
+   * This is useful for:
+   * - Lazy/on-demand queries that should wait for user interaction
+   * - Dependent queries that need data from another query first
+   * - Conditional queries based on component state
+   *
+   * @default true
+   * });
+   */
+  enabled?: boolean;
+
+  /**
+   * The default status of the users returned in the list.
+   *
+   * @default "ACTIVE"
+   */
+  include?: Core.UserStatus;
+
+  /**
+   * The preferred page size for the list.
+   *
+   * @default 1000
+   */
+  pageSize?: number;
+
+  /**
+   * The page token indicates where to start paging. This should be omitted from the first page's request.
+   * To fetch the next page, clients should take the value from the nextPageToken field of the previous
+   * response and use it to populate the pageToken field of the next request.
+   */
+  pageToken?: string;
+}
+
+export interface UseFoundryUsersListResult {
+  users: Admin.ListUsersResponse["data"] | undefined;
+  nextPageToken: string | undefined;
+  isLoading: boolean;
+
+  error: Error | undefined;
+
+  refetch: () => void;
+}
+
+export function useFoundryUsersList(
+  { enabled = true, include = "ACTIVE", pageSize = 1000, pageToken }:
+    UseCurrentFoundryUserOptions = {},
+): UseFoundryUsersListResult {
+  const { client } = React.useContext(OsdkContext2);
+
+  const handleQuery = React.useCallback(() => {
+    return Admin.Users.list(client, { include, pageSize, pageToken });
+  }, [client]);
+
+  const query = usePlatformQuery({
+    query: handleQuery,
+    enabled,
+  });
+
+  return {
+    users: query.data?.data,
+    nextPageToken: query.data?.nextPageToken,
+    isLoading: query.isLoading,
+    error: query.error,
+    refetch: query.refetch,
+  };
+}
