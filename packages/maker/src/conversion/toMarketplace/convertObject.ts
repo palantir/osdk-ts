e692bb73b64a997b27a40178234a6af7cbf0361e
Ethan Ai <ethanai22@gmail.com>
2026-01-28 16:02:18 -0500

    v0

--- packages/maker/src/conversion/toMarketplace/convertObject.ts
+++ packages/maker/src/conversion/toMarketplace/convertObject.ts
@@ -23,7 +23,6 @@ import type {
   OntologyIrPropertyType,
 } from "@osdk/client.unstable";
 import {
-  addNamespaceIfNone,
   buildDatasource,
   cleanAndValidateLinkTypeId,
   convertObjectStatus,
@@ -107,14 +106,16 @@ export function convertObject(
       implementsInterfaces2: implementations.map(impl => ({
         interfaceTypeApiName: impl.implements.apiName,
         linksV2: {},
-        propertiesV2: {},
-        properties: Object.fromEntries(
-          impl.propertyMapping.map(
-            mapping => [addNamespaceIfNone(mapping.interfaceProperty), {
-              propertyTypeRid: mapping.mapsTo,
-            }],
-          ),
-        ),
+        propertiesV2: Object.fromEntries(impl.propertyMapping
+        .map(
+          mappings => [mappings.interfaceProperty,
+            {
+              type: "propertyTypeRid",
+              propertyTypeRid: mappings.mapsTo,
+            }
+          ]
+        )),
+        properties: {},
       })),
       allImplementsInterfaces: {},
     },
