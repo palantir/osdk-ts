ea7a603a67be33f837fd57dc2219e51a5601899c
David Paquin <dpaquin@palantir.com>
2026-01-30 14:21:29 -0500

    validate no duplicate API name + version pairs

--- packages/maker/src/api/defineValueType.ts
+++ packages/maker/src/api/defineValueType.ts
@@ -25,7 +25,11 @@ import type {
 } from "@osdk/client.unstable";
 import invariant from "tiny-invariant";
 import { OntologyEntityTypeEnum } from "./common/OntologyEntityTypeEnum.js";
-import { namespace, updateOntology } from "./defineOntology.js";
+import {
+  namespace,
+  ontologyDefinition,
+  updateOntology,
+} from "./defineOntology.js";
 import { type ValueTypeDefinitionVersion } from "./values/ValueTypeDefinitionVersion.js";
 import { type ValueTypeType } from "./values/ValueTypeType.js";
 
@@ -146,6 +150,14 @@ export function defineValueType(
   const semverValidation =
     /^((([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)$/;
   invariant(semverValidation.test(version), "Version is not a valid semver");
+
+  const existingVersions =
+    ontologyDefinition[OntologyEntityTypeEnum.VALUE_TYPE][apiName] ?? [];
+  const duplicateVersion = existingVersions.find(vt => vt.version === version);
+  invariant(
+    duplicateVersion === undefined,
+    `Value type with apiName ${apiName} and version ${version} is already defined`,
+  );
 
   const typeName: TypeNames = typeof type.type === "string"
     ? type.type
