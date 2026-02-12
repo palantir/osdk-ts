fb5e8138bdb37d80a81e2b60eaa6097d2332a968
Kuber Sethi <ksethi@palantir.com>
2026-02-12 14:45:08 -0500

    delete unused useLatestRef and remove objectSet from UsePropertyAggregationOptions

--- packages/react-components/src/filter-list/hooks/usePropertyAggregation.ts
+++ packages/react-components/src/filter-list/hooks/usePropertyAggregation.ts
@@ -49,7 +49,7 @@ export function usePropertyAggregation<
 >(
   objectType: Q,
   propertyKey: K,
-  options?: UsePropertyAggregationOptions<Q>,
+  options?: UsePropertyAggregationOptions,
 ): UsePropertyAggregationResult {
   // AggregateOpts requires specific property keys from Q, but we're dynamically
   // using propertyKey. The cast is unavoidable for this dynamic filter pattern.
