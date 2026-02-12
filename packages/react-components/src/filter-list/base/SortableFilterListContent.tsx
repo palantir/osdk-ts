504871f4e0032ea3481d0b05b6a66d0244f7cb52
Kuber Sethi <ksethi@palantir.com>
2026-02-12 16:23:38 -0500

    remove unused ts-expect-error directives for dnd-kit types

--- packages/react-components/src/filter-list/base/SortableFilterListContent.tsx
+++ packages/react-components/src/filter-list/base/SortableFilterListContent.tsx
@@ -180,7 +180,6 @@ export default function SortableFilterListContent<
       className={classnames(contentStyles.content, className)}
       style={style}
     >
-      {/* @ts-expect-error dnd-kit types compiled against React 19, project uses React 18 */}
       <DndContext
         sensors={sensors}
         collisionDetection={closestCenter}
@@ -212,7 +211,6 @@ export default function SortableFilterListContent<
           })}
         </SortableContext>
 
-        {/* @ts-expect-error dnd-kit types compiled against React 19, project uses React 18 */}
         <DragOverlay
           dropAnimation={null}
           className={contentStyles.dragOverlay}
