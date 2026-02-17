d264cfe36aa08b427d95b83551222ad8049eec92
zpoh <zpoh@palantir.com>
2026-02-17 12:47:43 +0000

    Test build

--- packages/react-components-storybook/.storybook/main.ts
+++ packages/react-components-storybook/.storybook/main.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
@@ -19,6 +35,12 @@ const config: StorybookConfig = {
   },
   staticDirs: ["../public"],
   async viteFinal(config) {
+    // Set base path for GitHub Pages deployment
+    if (config.mode === "production") {
+      // Allow overriding base path via environment variable for previews
+      config.base = process.env.STORYBOOK_BASE_PATH || "/osdk-ts/storybook/";
+    }
+
     // Ensure proper resolution of workspace packages
     config.resolve = {
       ...config.resolve,
