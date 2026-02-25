5733353d977de31511f01d05368531734ebf346d
Kuber Sethi <ksethi@palantir.com>
2026-02-25 00:52:45 -0500

    fix sandbox imports to use FilterList instead of BaseFilterList

--- packages/e2e.sandbox.peopleapp/src/app/employees/EmployeesWithFilterList.tsx
+++ packages/e2e.sandbox.peopleapp/src/app/employees/EmployeesWithFilterList.tsx
@@ -16,8 +16,8 @@
 
 import type { WhereClause } from "@osdk/api";
 import {
-  BaseFilterList,
   type FilterDefinitionUnion,
+  FilterList,
 } from "@osdk/react-components/experimental";
 import { useOsdkObjects } from "@osdk/react/experimental";
 import "@osdk/react-components/styles.css";
@@ -89,7 +89,7 @@ export function EmployeesWithFilterList(props: EmployeesWithFilterListProps) {
     <>
       <div style={{ display: "flex", gap: "16px", height: "100%" }}>
         <div>
-          <BaseFilterList
+          <FilterList
             style={{ minWidth: 280, maxWidth: 320 }}
             objectSet={$(Employee)}
             filterDefinitions={filterDefinitions}
