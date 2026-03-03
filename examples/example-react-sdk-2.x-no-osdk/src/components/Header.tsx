0ac35b05302e4a53ee8dbfd4f26e4ae6f5e640f5
Sami Alfhaily <salfhaily@palantir.com>
2026-03-03 13:19:11 +0100

    Add blueprint to React template

--- examples/example-react-sdk-2.x-no-osdk/src/components/Header.tsx
+++ examples/example-react-sdk-2.x-no-osdk/src/components/Header.tsx
@@ -1,27 +1,26 @@
 import palantir from "/palantir.svg";
 import react from "/react.svg";
 
+import { H5, Link } from "@blueprintjs/core";
 import React from "react";
 import css from "./Header.module.css";
 
 function Header(): React.ReactElement {
   return (
-    <div className={css.header}>
+    <div className={css.headerContainer}>
       <div className={css.logos}>
-        <a
+        <Link
           href="https://www.palantir.com/docs/foundry/ontology-sdk/overview/"
           target="_blank"
           rel="noreferrer"
         >
           <img src={palantir} className={css.logo} alt="Palantir logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
+        </Link>
+        <Link href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={react} className={css.logo} alt="React logo" />
-        </a>
+        </Link>
       </div>
-      <h2 className={css.heading}>
-        Build your React application with Foundry!
-      </h2>
+      <H5 className={css.header}>Build your React application with Foundry!</H5>
     </div>
   );
 }
--- examples/example-react-sdk-2.x/src/components/NextSteps.tsx
+++ examples/example-react-sdk-2.x/src/components/NextSteps.tsx
@@ -1,25 +1,24 @@
-import tag from "/tag.svg";
-
+import { Code, H5, H6, Icon, Link, Text } from "@blueprintjs/core";
+import { type IconName } from "@blueprintjs/icons";
 import React from "react";
 import css from "./NextSteps.module.css";
 
 const STEPS: INextStep[] = [
   {
-    icon: tag,
+    icon: "tag",
     title: "Deploy your application",
     subtitle: (
       <span>
         To deploy a new version of your application, tag a new version of your
         repository or use the command line to run{" "}
-        <code className={css.code}>npx @osdk/cli@latest site deploy</code>.{" "}
-        <a
-          className={css.learnMore}
+        <Code>npx @osdk/cli@latest site deploy</Code>.{" "}
+        <Link
           href="https://www.npmjs.com/package/@osdk/cli"
           target="_blank"
           rel="noreferrer"
         >
           Learn more.
-        </a>
+        </Link>
       </span>
     ),
   },
@@ -28,7 +27,7 @@ const STEPS: INextStep[] = [
 function NextSteps(): React.ReactElement {
   return (
     <div className={css.nextSteps}>
-      <h5 className={css.nextStepsHeader}>Next steps</h5>
+      <H5 className={css.nextStepsHeader}>Next steps</H5>
       {STEPS.map((step) => (
         <NextStep
           key={step.title}
@@ -42,22 +41,20 @@ function NextSteps(): React.ReactElement {
 }
 
 interface INextStep {
-  icon: string;
+  icon: IconName;
   title: string;
   subtitle: React.ReactElement;
 }
 
-function NextStep({
-  icon,
-  title,
-  subtitle,
-}: INextStep): React.ReactElement {
+function NextStep({ icon, title, subtitle }: INextStep): React.ReactElement {
   return (
     <div className={css.nextStep}>
-      <img src={icon} className={css.icon} alt={icon} />
+      <div className={css.icon}>
+        <Icon icon={icon} aria-label={icon} />
+      </div>
       <div className={css.nextStepContent}>
-        <h6 className={css.title}>{title}</h6>
-        <div className={css.subtitle}>{subtitle}</div>
+        <H6 className={css.title}>{title}</H6>
+        <Text className={css.subtitle}>{subtitle}</Text>
       </div>
     </div>
   );
