05803ac91565827bf810d9081abbf4ff9ef1be55
Kuber Sethi <ksethi@palantir.com>
2026-03-18 14:43:24 -0400

    add dx improvements to react.beta template
    • add @/* path aliases, ErrorBoundary, Suspense wrapping, and Loading fallback
    • add stricter eslint rules, prettier import sorting, check script, and editorconfig
    • add vscode settings/extensions recommendations

--- packages/create-app.template.react.beta/templates/prettier.config.js
+++ packages/create-app.template.react.beta/templates/prettier.config.js
@@ -4,4 +4,14 @@ export default {
   trailingComma: "all",
   printWidth: 100,
   tabWidth: 2,
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  importOrder: [
+    "^react",
+    "^@osdk/(.*)$",
+    "<THIRD_PARTY_MODULES>",
+    "^@/(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
 };
