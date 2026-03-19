933fda8780acf4f4594df3a60fa2fb2880e1c175
Kuber Sethi <ksethi@palantir.com>
2026-03-19 18:00:27 -0400

    remove unused React import from main.tsx template

--- examples/example-react-sdk-2.x-no-osdk/src/main.tsx
+++ examples/example-react-sdk-2.x-no-osdk/src/main.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
