c201a161b96e12e3c49a513fae56a42e6580426b
nadira pranatio <npranatio@palantir.com>
2026-03-20 09:55:03 -0400

    add link to osdk docs

--- examples/example-expo-sdk-2.x-no-osdk/foundry/client.ts
+++ examples/example-expo-sdk-2.x-no-osdk/foundry/client.ts
@@ -9,5 +9,9 @@ import { getValidAuthToken } from "./Auth";
 
 /**
  * Initialize the client to interact with the Platform SDK
+ *
+ * If you later add an Ontology SDK to your application, follow the steps in 
+ * https://fake.palantirfoundry.com/docs/foundry/ontology-sdk/add-osdk-to-bootstrapped-repository/
+ * to correctly set it up in this code repository.
  */
 export const client: PlatformClient = createPlatformClient(FOUNDRY_URL, getValidAuthToken);
--- examples/example-react-sdk-2.x-no-osdk/src/client.ts
+++ examples/example-react-sdk-2.x-no-osdk/src/client.ts
@@ -37,6 +37,10 @@ export const auth: PublicOauthClient = createPublicOauthClient(
 
 /**
  * Initialize the client to interact with the Platform SDK
+ *
+ * If you later add an Ontology SDK to your application, follow the steps in 
+ * https://fake.palantirfoundry.com/docs/foundry/ontology-sdk/add-osdk-to-bootstrapped-repository/
+ * to correctly set it up in this code repository.
  */
 export const client: PlatformClient = createPlatformClient(foundryUrl, auth);
 
