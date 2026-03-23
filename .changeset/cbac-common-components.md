7885fbd0effaaebf8d129ac21c02fa65acbd510f
Kuber Sethi <ksethi@palantir.com>
2026-03-23 17:24:10 -0400

    rename react-components-defense to cbac-components

--- .changeset/cbac-common-components.md
+++ .changeset/cbac-common-components.md
@@ -1,7 +1,7 @@
 ---
 "@osdk/react-components": patch
 "@osdk/react-components-styles": patch
-"@osdk/react-components-defense": minor
+"@osdk/cbac-components": minor
 ---
 
-add cbac types, selection logic, css tokens, and create react-components-defense package
+add cbac types, selection logic, css tokens, and create cbac-components package
--- packages/cbac-components/scripts/build-css.mjs
+++ packages/cbac-components/scripts/build-css.mjs
@@ -77,8 +77,7 @@ async function rewriteCssImports() {
 
 async function processCssModules() {
   const cssFiles = await findCssModules(buildDir);
-  let combinedCss =
-    "/* @osdk/react-components-defense - Combined styles */\n\n";
+  let combinedCss = "/* @osdk/cbac-components - Combined styles */\n\n";
 
   for (const cssFile of cssFiles) {
     const content = await fs.readFile(cssFile, "utf8");
