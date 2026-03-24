0689a9a074cd2bb7d0e7e7f2646861fa32b4d3dc
Ollie Jennings <ojennings@palantir.com>
2026-03-24 18:02:57 +0000

    more hook tests and moved current tests

--- packages/react-components/src/pdf-viewer/components/__tests__/PdfViewerSearchBar.test.tsx
+++ packages/react-components/src/pdf-viewer/components/__tests__/PdfViewerSearchBar.test.tsx
@@ -16,7 +16,7 @@
 
 import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 import { afterEach, describe, expect, it, vi } from "vitest";
-import { PdfViewerSearchBar } from "../components/PdfViewerSearchBar.js";
+import { PdfViewerSearchBar } from "../PdfViewerSearchBar.js";
 
 const defaultProps = {
   query: "",
