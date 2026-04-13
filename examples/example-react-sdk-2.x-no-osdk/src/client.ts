8bac705a10ecf77a70a7147fe08d0ca9e46a958b
Alexandru Beloiu <beloiual@umich.edu>
2026-04-13 09:34:41 -0400

    snapshot

--- examples/example-react-sdk-2.x-no-osdk/src/client.ts
+++ examples/example-react-sdk-2.x-no-osdk/src/client.ts
@@ -38,7 +38,7 @@ export const auth: PublicOauthClient = createPublicOauthClient(
 /**
  * Initialize the client to interact with the Platform SDK
  *
- * If you later add an Ontology SDK to your application, follow the steps in 
+ * If you later add an Ontology SDK to your application, follow the steps in
  * https://fake.palantirfoundry.com/docs/foundry/ontology-sdk/add-osdk-to-bootstrapped-repository/
  * to correctly set it up in this project.
  */
