1aebfb3710313ffd2651112bd526b6c3247a7e8b
Ollie Jennings <ojennings@palantir.com>
2026-04-21 13:37:32 +0100

    Add transpileBrowser dep and react-components subpath imports
    - Add ^transpileBrowser to turbo task deps since webpack resolves
      the browser export condition
    - Import all react-components subpath exports (experimental/*,
      primitives) to verify they bundle correctly

--- tests/verify-webpack-bundling/src/entry.ts
+++ tests/verify-webpack-bundling/src/entry.ts
@@ -7,6 +7,14 @@ import * as Client from "@osdk/client";
 import * as OAuth from "@osdk/oauth";
 import * as OsdkReact from "@osdk/react";
 import * as ReactComponents from "@osdk/react-components";
+import * as ReactComponentsExperimental from "@osdk/react-components/experimental";
+import * as ReactComponentsPrimitives from "@osdk/react-components/primitives";
+import * as ReactComponentsActionForm from "@osdk/react-components/experimental/action-form";
+import * as ReactComponentsFilterList from "@osdk/react-components/experimental/filter-list";
+import * as ReactComponentsMarkdown from "@osdk/react-components/experimental/markdown-renderer";
+import * as ReactComponentsObjectTable from "@osdk/react-components/experimental/object-table";
+import * as ReactComponentsPdfViewer from "@osdk/react-components/experimental/pdf-viewer";
+import * as ReactComponentsTiffRenderer from "@osdk/react-components/experimental/tiff-renderer";
 import * as WidgetClientReact from "@osdk/widget.client-react";
 
 // Re-export to prevent tree-shaking from removing the imports
@@ -16,5 +24,13 @@ export {
   OAuth,
   OsdkReact,
   ReactComponents,
+  ReactComponentsActionForm,
+  ReactComponentsExperimental,
+  ReactComponentsFilterList,
+  ReactComponentsMarkdown,
+  ReactComponentsObjectTable,
+  ReactComponentsPdfViewer,
+  ReactComponentsPrimitives,
+  ReactComponentsTiffRenderer,
   WidgetClientReact,
 };
