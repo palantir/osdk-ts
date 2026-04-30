d6abc43dcde60aa7fed1d339f3040f319650761a
Ndeye Fatou Diop <ndeye-fatou.diop@berkeley.edu>
2026-04-30 09:19:04 +0100

    Add Component Showcase story with tabbed layout (#3128)
    Co-authored-by: nfdiop <nfdiop@palantir.com>

---
+++ packages/react-components-storybook/src/stories/ComponentShowcase/ComponentShowcase.stories.tsx
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2026 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { fauxFoundry } from "../../mocks/fauxFoundry.js";
+import { ComponentShowcase } from "./ComponentShowcase.js";
+
+const meta: Meta<typeof ComponentShowcase> = {
+  title: "Component Showcase",
+  component: ComponentShowcase,
+  parameters: {
+    layout: "fullscreen",
+    docs: { disable: true },
+    controls: { disable: true },
+    msw: {
+      handlers: fauxFoundry.handlers,
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
