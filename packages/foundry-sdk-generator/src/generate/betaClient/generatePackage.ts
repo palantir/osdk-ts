913563fe8687062b64e746bdeaedd93fbd3a62f8
Eunsoo Shin <62971511+esinx@users.noreply.github.com>
2026-07-28 10:09:51 -0400

    Export metadata as json module (#3753)
    * export metadata as json module
    
    * remove deps from generated package
    
    * add snapshot tests
    
    * Add snapshot tests for package.json and ontology-metadata.json
    
    * convert non-snapshot test to include verbose string

--- packages/foundry-sdk-generator/src/generate/betaClient/generatePackage.ts
+++ packages/foundry-sdk-generator/src/generate/betaClient/generatePackage.ts
@@ -28,6 +28,11 @@ import { USER_AGENT } from "../../utils/UserAgent.js";
 import { generateBundles } from "../generateBundles.js";
 import { bundleDependencies } from "./bundleDependencies.js";
 import { compileInMemory } from "./compileInMemory.js";
+import {
+  generateOntologyMetadata,
+  ONTOLOGY_METADATA_DTS_PATH,
+  ONTOLOGY_METADATA_JSON_PATH,
+} from "./generateOntologyMetadata.js";
 import { generatePackageJson } from "./generatePackageJson.js";
 
 const betaPeerDependencies: { [key: string]: string | undefined } = {
@@ -92,6 +97,12 @@ export async function generatePackage(
     false,
     ontologyInfo.fixedVersionQueryTypes,
   );
+
+  await generateOntologyMetadata({
+    metadata: ontologyInfo.requestedMetadata,
+    path: join(packagePath, ONTOLOGY_METADATA_JSON_PATH),
+    typePath: join(packagePath, ONTOLOGY_METADATA_DTS_PATH),
+  });
 
   // actually write file plus save contents
   const contents = await generatePackageJson({
