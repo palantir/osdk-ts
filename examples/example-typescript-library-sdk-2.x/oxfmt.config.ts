8f086809b3d354479cf46870471a5a19762c8ced
nfdiop <nfdiop@palantir.com>
2026-07-30 11:50:38 +0200

    Revert trailingComma override on generated example config

--- examples/example-typescript-library-sdk-2.x/oxfmt.config.ts
+++ examples/example-typescript-library-sdk-2.x/oxfmt.config.ts
@@ -3,8 +3,6 @@ import ultracite from "ultracite/oxfmt";
 
 export default defineConfig({
   ...ultracite,
-  // Override Ultracite's `trailingComma: "es5"` to match the monorepo root config.
-  trailingComma: "all",
   ignorePatterns: [
     ...(ultracite.ignorePatterns ?? []),
     "**/package.json",
