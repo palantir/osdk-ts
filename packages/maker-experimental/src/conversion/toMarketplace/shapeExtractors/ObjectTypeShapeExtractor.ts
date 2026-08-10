3fd405d9ffae89c299d42e209f13ef62b935e819
Alex Parson <aparson@palantir.com>
2026-08-10 16:56:14 +0100

    Support edit-only properties in Maker Experimental

--- packages/maker-experimental/src/conversion/toMarketplace/shapeExtractors/ObjectTypeShapeExtractor.ts
+++ packages/maker-experimental/src/conversion/toMarketplace/shapeExtractors/ObjectTypeShapeExtractor.ts
@@ -272,7 +272,7 @@ export class ObjectTypeShapeExtractor {
           columnReadableIds,
           dsDefinition.datasetV2.datasetRid,
           dsDefinition.datasetV2.branchId,
-          dsDefinition.datasetV2.propertyMapping,
+          this.getColumnMappings(dsDefinition.datasetV2.propertyMapping),
           ridGenerator,
         );
       case "datasetV3":
@@ -284,7 +284,7 @@ export class ObjectTypeShapeExtractor {
           columnReadableIds,
           dsDefinition.datasetV3.datasetRid,
           dsDefinition.datasetV3.branchId,
-          dsDefinition.datasetV3.propertyMapping,
+          this.getColumnMappings(dsDefinition.datasetV3.propertyMapping),
           ridGenerator,
         );
       case "derived":
@@ -317,7 +317,7 @@ export class ObjectTypeShapeExtractor {
           propertyOutputShapeMap,
           columnReadableIds,
           dsDefinition.restrictedViewV2.restrictedViewRid,
-          dsDefinition.restrictedViewV2.propertyMapping,
+          this.getColumnMappings(dsDefinition.restrictedViewV2.propertyMapping),
           ridGenerator,
         );
       case "stream":
@@ -377,6 +377,16 @@ export class ObjectTypeShapeExtractor {
       default:
         return new Map();
     }
+  }
+
+  private getColumnMappings(
+    propertyMappings: Record<PropertyTypeRid, PropertyTypeMappingInfo>,
+  ): Record<PropertyTypeRid, PropertyTypeMappingInfo> {
+    return Object.fromEntries(
+      Object.entries(propertyMappings).filter(
+        ([, mapping]) => mapping.type === "column" || mapping.type === "struct",
+      ),
+    );
   }
 
   private getShapesFromGeotimeSeriesIntegration(
