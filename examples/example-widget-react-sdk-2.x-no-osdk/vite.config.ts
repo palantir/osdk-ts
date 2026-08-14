8718d53b7732f75b01252c7e08f65fb27c45e9aa
Tim Leung <tleung@palantir.com>
2026-08-14 13:14:17 +0100

    create-widget react template support no osdk (#3849)

---
+++ examples/example-widget-react-sdk-2.x-no-osdk/vite.config.ts
@@ -0,0 +1,17 @@
+import foundryWidgetPlugin from "@osdk/widget.vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), foundryWidgetPlugin()],
+  server: {
+    port: 8080,
+    cors: true,
+  },
+  build: {
+    rollupOptions: {
+      input: ["./index.html"],
+    },
+  },
+});
